a77c6873ab7bc61ab1414dddf1c693cee346fc2c
Milos Tomic <milos.tomic@sap.com>
2024-03-05 16:26:40 +0100

    scope fix upon deployment - in Kyma CR module first appears in the status and not in spec

--- pkg/kcp/scope/findKymaModuleState.go
+++ pkg/kcp/scope/findKymaModuleState.go
@@ -9,8 +9,12 @@ import (
 func findKymaModuleState(ctx context.Context, st composed.State) (error, context.Context) {
 	state := st.(*State)
 
-	isListed := util.IsKymaModuleListedInSpec(state.kyma, "cloud-manager")
-
+	// Once module is added to the SKR Kyma CR, in KCP it first appears in the status field
+	// with state: Processing, and it does not appear in the spec
+	moduleState := util.GetKymaModuleStateFromStatus(state.kyma, "cloud-manager")
+	//isListed := util.IsKymaModuleListedInSpec(state.kyma, "cloud-manager")
+	isListed := moduleState != util.KymaModuleStateNotPresent
+	
 	if !isListed {
 		return composed.StopAndForget, nil
 	}
