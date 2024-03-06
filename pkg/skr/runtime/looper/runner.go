4ec675b132be2c1ea23e3ffadc5b292609354c5b
Milos Tomic <milos.tomic@sap.com>
2024-03-06 10:06:40 +0100

    fixes

--- pkg/skr/runtime/looper/runner.go
+++ pkg/skr/runtime/looper/runner.go
@@ -91,8 +91,6 @@ func (r *skrRunner) Run(ctx context.Context, skrManager skrmanager.SkrManager, o
 
 		if options.provider != nil {
 			logger.Info(fmt.Sprintf("This SKR cluster is started with provider option %s", *options.provider))
-			// TODO: remove me
-			logger.Info("Dummy test, TODO: remove me")
 			instlr := &installer{
 				skrProvidersPath: os.Getenv("SKR_PROVIDERS"),
 				logger:           logger,
