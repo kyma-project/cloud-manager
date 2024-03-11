bc2fe1ce55787b323677834610f2e17951728680
Vladimir Andjelkoski <vladimir.andjelkoski@sap.com>
2024-03-11 21:11:52 +0100

    cli kyma create

--- cmd/cli/cmdKymaCreate.go
+++ cmd/cli/cmdKymaCreate.go
@@ -3,10 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/google/uuid"
 	"github.com/kyma-project/cloud-manager/cmd/cli/helper"
 	"github.com/kyma-project/cloud-manager/pkg/util"
 	"github.com/spf13/cobra"
-	"strings"
 )
 
 func init() {
@@ -21,21 +21,27 @@ var cmdKymaCreate = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) error {
 		c := helper.NewKcpClient()
 
-		list := util.NewKymaListUnstructured()
-		err := c.List(context.Background(), list)
+		kyma := util.NewKymaUnstructured()
+
+		if kymaName == "" {
+			fmt.Println("Kyma name not specified it will be generated.")
+			kymaName = uuid.NewString()
+		}
+		kyma.SetName(kymaName)
+		kyma.SetNamespace("kcp-system")
+
+		if shootName != "" {
+			fmt.Println("Adding kyma-project.io/shoot-name label")
+			kyma.SetLabels(map[string]string{"kyma-project.io/shoot-name": shootName})
+		}
+
+		err := c.Create(context.Background(), kyma)
+
 		if err != nil {
-			return fmt.Errorf("error listing Kymas: %w", err)
+			return fmt.Errorf("error creating Kyma: %w", err)
 		}
 
-		fmt.Printf("KymaName \t\t\t\t\t\t Module %s state\n", kymaModuleName)
-		for _, item := range list.Items {
-			state := util.GetKymaModuleStateFromStatus(&item, kymaModuleName)
-			name := item.GetName()
-			if kymaName == "" || strings.Contains(name, kymaName) {
-				fmt.Printf("%s/%s \t %s\n", item.GetNamespace(), name, state)
-			}
-		}
-
+		fmt.Printf("Created Kyma %v\n", kymaName)
 		return nil
 	},
 }
