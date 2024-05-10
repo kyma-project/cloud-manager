0f128aa74b125848d7e565294f39676ce5e4e982
Milos Tomic <milos.tomic@sap.com>
2024-05-10 07:52:45 +0200

    fix(AwsNfsVolume): wait until mount targets are available

--- internal/controller/cloud-resources/quotaIpRange/suite_test.go
+++ internal/controller/cloud-resources/quotaIpRange/suite_test.go
@@ -5,6 +5,7 @@ import (
 	cloudresourcesv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-resources/v1beta1"
 	cloudresourcescontroller "github.com/kyma-project/cloud-manager/internal/controller/cloud-resources"
 	"github.com/kyma-project/cloud-manager/pkg/quota"
+	"github.com/kyma-project/cloud-manager/pkg/util"
 	"os"
 	"testing"
 
@@ -51,6 +52,8 @@ var _ = BeforeSuite(func() {
 
 	// Quota override
 	quota.SkrQuota.Override(&cloudresourcesv1beta1.IpRange{}, infra.SKR().Scheme(), "", 1)
+
+	util.SetSpeedyTimingForTests()
 
 	// Setup controllers
 	// IpRange
