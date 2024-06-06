ce68accc4dea325799aa0df71b3024b0de619fb9
Ravi Shankar <ravi.shankar06@sap.com>
2024-06-06 15:23:46 -0700

    chore: Remove Disabled CRDs flag

--- internal/controller/cloud-resources/gcpnfsvolumebackup_controller.go
+++ internal/controller/cloud-resources/gcpnfsvolumebackup_controller.go
@@ -20,8 +20,6 @@ import (
 	"context"
 	"github.com/go-logr/logr"
 	cloudresourcesv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-resources/v1beta1"
-	"github.com/kyma-project/cloud-manager/pkg/util"
-
 	"github.com/kyma-project/cloud-manager/pkg/common/abstractions"
 	gcpclient "github.com/kyma-project/cloud-manager/pkg/kcp/provider/gcp/client"
 	backupclient "github.com/kyma-project/cloud-manager/pkg/kcp/provider/gcp/nfsbackup/client"
@@ -72,14 +70,9 @@ func (f *GcpNfsVolumeBackupReconcilerFactory) New(args reconcile2.ReconcilerArgu
 
 func SetupGcpNfsVolumeBackupReconciler(reg skrruntime.SkrRegistry, fileBackupClientProvider gcpclient.ClientProvider[backupclient.FileBackupClient],
 	env abstractions.Environment, logger logr.Logger) error {
-	// "_" + crd + ".yaml" should be the suffix for the yaml present in config/crd/bases
-	if util.IsCrdDisabled(env, "GcpNfsVolumeBackups") {
-		logger.Info("GcpNfsVolumeBackup CRD is disabled. Skipping controller setup.")
-		return nil
-	} else {
-		return reg.Register().
-			WithFactory(&GcpNfsVolumeBackupReconcilerFactory{fileBackupClientProvider: fileBackupClientProvider, env: env}).
-			For(&cloudresourcesv1beta1.GcpNfsVolumeBackup{}).
-			Complete()
-	}
+
+	return reg.Register().
+		WithFactory(&GcpNfsVolumeBackupReconcilerFactory{fileBackupClientProvider: fileBackupClientProvider, env: env}).
+		For(&cloudresourcesv1beta1.GcpNfsVolumeBackup{}).
+		Complete()
 }
