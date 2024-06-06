13b517f3c5271c38ca2267985d0dff8525af37c4
Ivan Jovovic <ivan.jovovic@sap.com>
2024-06-06 11:34:48 +0200

    loadPersistentVOlumeClaim: remove backward compatibility code + fix the Name value when loading the PVC

--- pkg/skr/awsnfsvolume/loadVolumeClaim.go
+++ pkg/skr/awsnfsvolume/loadVolumeClaim.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"github.com/kyma-project/cloud-manager/pkg/composed"
 	corev1 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -15,24 +14,10 @@ func loadPersistentVolumeClaim(ctx context.Context, st composed.State) (error, c
 	pvc := &corev1.PersistentVolumeClaim{}
 	err := state.Cluster().K8sClient().Get(ctx, types.NamespacedName{
 		Namespace: state.Obj().GetNamespace(),
-		Name:      getVolumeName(state.ObjAsAwsNfsVolume()),
+		Name:      getVolumeClaimName(state.ObjAsAwsNfsVolume()),
 	}, pvc)
 	if client.IgnoreNotFound(err) != nil {
-		return composed.LogErrorAndReturn(err, "Error getting PersistentVolumeClaim by getVolumeName()", composed.StopWithRequeue, ctx)
-	}
-
-	if apierrors.IsNotFound(err) {
-		// first PVs were created with name = AwsNfsVolume.status.id
-		// next, a feature was added in AwsNfsVolume to specify PV name
-		// this is a fallback to old behavior where PV.name = AwsNfsVolume.status.id
-		// to remain compatibility with already created PVs
-		err = state.Cluster().K8sClient().Get(ctx, types.NamespacedName{
-			Namespace: state.Obj().GetNamespace(),
-			Name:      state.ObjAsAwsNfsVolume().Status.Id,
-		}, pvc)
-		if client.IgnoreNotFound(err) != nil {
-			return composed.LogErrorAndReturn(err, "Error getting PersistentVolume by status.id", composed.StopWithRequeue, ctx)
-		}
+		return composed.LogErrorAndReturn(err, "Error getting PersistentVolumeClaim by getVolumeClaimName()", composed.StopWithRequeue, ctx)
 	}
 
 	if err == nil {
