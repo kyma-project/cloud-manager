1696626841c46ecb83a6d2ed90e27acaefec4010
Dušan Pantić <dushanpantic@users.noreply.github.com>
2024-06-12 16:20:28 +0200

    add more logs to modify persistence volume action (#283)

--- pkg/skr/gcpnfsvolume/modifyPersistenceVolume.go
+++ pkg/skr/gcpnfsvolume/modifyPersistenceVolume.go
@@ -2,16 +2,17 @@ package gcpnfsvolume
 
 import (
 	"context"
+	"reflect"
+	"time"
+
 	"github.com/kyma-project/cloud-manager/api/cloud-resources/v1beta1"
 	"github.com/kyma-project/cloud-manager/pkg/composed"
 	"k8s.io/apimachinery/pkg/api/meta"
-	"k8s.io/apimachinery/pkg/api/resource"
-	"reflect"
-	"time"
 )
 
 func modifyPersistenceVolume(ctx context.Context, st composed.State) (error, context.Context) {
 	state := st.(*State)
+	logger := composed.LoggerFromCtx(ctx)
 
 	//If GcpNfsVolume is marked for deletion, continue
 	if composed.MarkedForDeletionPredicate(ctx, st) {
@@ -21,7 +22,7 @@ func modifyPersistenceVolume(ctx context.Context, st composed.State) (error, con
 
 	//Get GcpNfsVolume object
 	nfsVolume := state.ObjAsGcpNfsVolume()
-	capacity := resource.NewQuantity(int64(nfsVolume.Spec.CapacityGb)*1024*1024*1024, resource.BinarySI)
+	capacity := gcpNfsVolumeCapacityToResourceQuantity(nfsVolume)
 
 	//If GcpNfsVolume is not Ready state, continue.
 	if !meta.IsStatusConditionTrue(nfsVolume.Status.Conditions, v1beta1.ConditionTypeReady) {
@@ -38,6 +39,7 @@ func modifyPersistenceVolume(ctx context.Context, st composed.State) (error, con
 	if !capacity.Equal(state.PV.Spec.Capacity["storage"]) {
 		changed = true
 		state.PV.Spec.Capacity["storage"] = *capacity
+		logger.Info("Detected modified PV capacity")
 	}
 
 	//If labels are different, update PV labels.
@@ -45,6 +47,7 @@ func modifyPersistenceVolume(ctx context.Context, st composed.State) (error, con
 	if !reflect.DeepEqual(state.PV.Labels, labels) {
 		changed = true
 		state.PV.Labels = labels
+		logger.Info("Detected modified PV labels")
 	}
 
 	//If annotations are different, update PV annotations.
@@ -52,6 +55,7 @@ func modifyPersistenceVolume(ctx context.Context, st composed.State) (error, con
 	if !reflect.DeepEqual(state.PV.Annotations, annotations) {
 		changed = true
 		state.PV.Annotations = annotations
+		logger.Info("Detected modified PV annotations")
 	}
 
 	//No changes to PV, continue.
