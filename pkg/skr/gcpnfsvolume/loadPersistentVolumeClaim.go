ba228c1b1332ffef73a24b54d94360bf1f9b74e5
Dušan Pantić <dusan.pantic@sap.com>
2024-06-14 11:50:54 +0200

    fix(GcpNfsVolume): ensure Error state is set if GcpNfsVolume specifies PVC name that already exists

--- pkg/skr/gcpnfsvolume/loadPersistentVolumeClaim.go
+++ pkg/skr/gcpnfsvolume/loadPersistentVolumeClaim.go
@@ -2,9 +2,12 @@ package gcpnfsvolume
 
 import (
 	"context"
+	"fmt"
 
+	cloudresourcesv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-resources/v1beta1"
 	"github.com/kyma-project/cloud-manager/pkg/composed"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -12,18 +15,40 @@ import (
 func loadPersistentVolumeClaim(ctx context.Context, st composed.State) (error, context.Context) {
 	state := st.(*State)
 
+	gcpNfsVolume := state.ObjAsGcpNfsVolume()
+
 	pvc := &corev1.PersistentVolumeClaim{}
 	err := state.Cluster().K8sClient().Get(ctx, types.NamespacedName{
 		Namespace: state.Obj().GetNamespace(),
-		Name:      getVolumeClaimName(state.ObjAsGcpNfsVolume()),
+		Name:      getVolumeClaimName(gcpNfsVolume),
 	}, pvc)
+
 	if client.IgnoreNotFound(err) != nil {
 		return composed.LogErrorAndReturn(err, "Error getting PersistentVolumeClaim by getVolumeName()", composed.StopWithRequeue, ctx)
 	}
 
-	if err == nil {
-		state.PVC = pvc
+	if err != nil { // PVC not-found
+		return nil, nil
 	}
+
+	pvcLabels := pvc.Labels
+	parentGcpNfsVolumeName := pvcLabels[cloudresourcesv1beta1.LabelNfsVolName]
+
+	if parentGcpNfsVolumeName != gcpNfsVolume.Name {
+		errorMsg := fmt.Sprintf("Loaded PVC(%s/%s) belongs to another GcpNfsVolume (%s)", pvc.Namespace, pvc.Name, parentGcpNfsVolumeName)
+		return composed.UpdateStatus(state.ObjAsGcpNfsVolume()).
+			SetCondition(metav1.Condition{
+				Type:    cloudresourcesv1beta1.ConditionTypeError,
+				Status:  metav1.ConditionTrue,
+				Reason:  cloudresourcesv1beta1.ConditionPVCBelongsToAnotherGcpNfsVolume,
+				Message: errorMsg,
+			}).
+			RemoveConditions(cloudresourcesv1beta1.ConditionTypeReady).
+			ErrorLogMessage(errorMsg).
+			Run(ctx, state)
+	}
+
+	state.PVC = pvc
 
 	return nil, nil
 }
