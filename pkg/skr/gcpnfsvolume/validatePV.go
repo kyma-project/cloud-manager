b36eec0912215fa44779bd4fe3764925b8a88d28
Dušan Pantić <dushanpantic@users.noreply.github.com>
2024-06-20 10:58:05 +0200

    fix(GcpNfsVolume): fix validatePV and add tests for it (#299)

--- pkg/skr/gcpnfsvolume/validatePV.go
+++ pkg/skr/gcpnfsvolume/validatePV.go
@@ -3,6 +3,7 @@ package gcpnfsvolume
 import (
 	"context"
 	"fmt"
+
 	cloudresourcesv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-resources/v1beta1"
 	"github.com/kyma-project/cloud-manager/pkg/composed"
 	corev1 "k8s.io/api/core/v1"
@@ -15,36 +16,31 @@ func validatePV(ctx context.Context, st composed.State) (error, context.Context)
 	state := st.(*State)
 	nfsVolume := state.ObjAsGcpNfsVolume()
 
-	//If marked for deletion, continue without validation.
-	if !composed.MarkedForDeletionPredicate(ctx, st) {
+	if composed.MarkedForDeletionPredicate(ctx, st) {
 		return nil, nil
 	}
 
-	//Get the PV with the desired name
 	pvName := getVolumeName(nfsVolume)
 	pv := &corev1.PersistentVolume{}
-	err := state.SkrCluster.K8sClient().Get(ctx, types.NamespacedName{Name: pvName}, pv)
+	err := state.Cluster().K8sClient().Get(ctx, types.NamespacedName{Name: pvName}, pv)
 
-	//If PV does not exist, continue.
 	if apierrors.IsNotFound(err) {
 		return nil, nil
 	}
 
-	//If some other error, stop and requeue
 	if err != nil {
 		return composed.LogErrorAndReturn(err, "Error getting PersistentVolume by name", composed.StopWithRequeue, ctx)
 	}
 
-	//If PV belongs to this NfsVolume, continue
-	name, nmExists := pv.Labels[cloudresourcesv1beta1.LabelNfsVolName]
-	ns, nsExists := pv.Labels[cloudresourcesv1beta1.LabelNfsVolNS]
-	if nmExists && nsExists && name == nfsVolume.Name && ns == nfsVolume.Namespace {
+	parentName, parentNameExists := pv.Labels[cloudresourcesv1beta1.LabelNfsVolName]
+	parentNamespace, parentNamespaceExists := pv.Labels[cloudresourcesv1beta1.LabelNfsVolNS]
+	if parentNameExists && parentNamespaceExists && parentName == nfsVolume.Name && parentNamespace == nfsVolume.Namespace {
 		return nil, nil
 	}
 
-	//If PV exists with the same name, but does not belong to this NfsVolume, then mark it as an error
-	state.ObjAsGcpNfsVolume().Status.State = cloudresourcesv1beta1.GcpNfsVolumeError
-	return composed.UpdateStatus(state.ObjAsGcpNfsVolume()).
+	nfsVolume.Status.State = cloudresourcesv1beta1.GcpNfsVolumeError
+
+	return composed.UpdateStatus(nfsVolume).
 		SetExclusiveConditions(metav1.Condition{
 			Type:    cloudresourcesv1beta1.ConditionTypeError,
 			Status:  metav1.ConditionTrue,
---
+++ pkg/skr/gcpnfsvolume/validatePV_test.go
@@ -0,0 +1,137 @@
+package gcpnfsvolume
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	cloudresourcesv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-resources/v1beta1"
+	composed "github.com/kyma-project/cloud-manager/pkg/composed"
+	spy "github.com/kyma-project/cloud-manager/pkg/testinfra/clientspy"
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+func TestValidatePV(t *testing.T) {
+
+	t.Run("validatePV", func(t *testing.T) {
+
+		var gcpNfsVolume *cloudresourcesv1beta1.GcpNfsVolume
+		var pv *corev1.PersistentVolume
+		var state *State
+		var k8sClient client.WithWatch
+
+		createEmptyGcpNfsVolumeState := func(k8sClient client.WithWatch, gcpNfsVolume *cloudresourcesv1beta1.GcpNfsVolume) *State {
+			cluster := composed.NewStateCluster(k8sClient, k8sClient, nil, k8sClient.Scheme())
+			return &State{
+				State: composed.NewStateFactory(cluster).NewState(types.NamespacedName{}, gcpNfsVolume),
+			}
+		}
+
+		setupTest := func() {
+			gcpNfsVolume = &cloudresourcesv1beta1.GcpNfsVolume{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test-gcpnfsvol",
+					Namespace: "test-ns",
+				},
+				Spec: cloudresourcesv1beta1.GcpNfsVolumeSpec{
+					CapacityGb: 1000,
+				},
+			}
+
+			pv = &corev1.PersistentVolume{
+				ObjectMeta: v1.ObjectMeta{
+					Name:   "test-gcpnfsvol",
+					Labels: getVolumeLabels(gcpNfsVolume),
+				},
+			}
+
+			scheme := runtime.NewScheme()
+			utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+			utilruntime.Must(cloudresourcesv1beta1.AddToScheme(scheme))
+			fakeClient := fake.NewClientBuilder().
+				WithScheme(scheme).
+				WithObjects(pv).
+				WithStatusSubresource(pv).
+				Build()
+
+			k8sClient = spy.NewClientSpy(fakeClient)
+
+			state = createEmptyGcpNfsVolumeState(k8sClient, gcpNfsVolume)
+			state.PV = &corev1.PersistentVolume{}
+		}
+
+		t.Run("Should: do nothing if GcpNfsVolume is marked for deletion", func(t *testing.T) {
+			setupTest()
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			gcpNfsVolume.ObjectMeta.DeletionTimestamp = &v1.Time{
+				Time: time.Now(),
+			}
+
+			err, res := validatePV(ctx, state)
+
+			assert.Nil(t, res, "should return nil res")
+			assert.Nil(t, err, "should return nil err")
+		})
+
+		t.Run("Should: do nothing if APIServer cant find requested PV", func(t *testing.T) {
+			setupTest()
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			scheme := runtime.NewScheme()
+			utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+			utilruntime.Must(cloudresourcesv1beta1.AddToScheme(scheme))
+			fakeClient := fake.NewClientBuilder().
+				WithScheme(scheme).
+				Build()
+			k8sClient.(spy.ClientSpy).SetClient(fakeClient)
+
+			err, res := validatePV(ctx, state)
+
+			assert.Nil(t, res, "should return nil res")
+			assert.Nil(t, err, "should return nil err")
+		})
+
+		t.Run("Should: do nothing if found PV has expected labels", func(t *testing.T) {
+			setupTest()
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			err, res := validatePV(ctx, state)
+
+			assert.Nil(t, res, "should return nil res")
+			assert.Nil(t, err, "should return nil err")
+		})
+
+		t.Run("Should: set Status to Error and returns error when PV belongs to another GcpNfsVolume", func(t *testing.T) {
+			setupTest()
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			pv.Labels[cloudresourcesv1beta1.LabelNfsVolName] = "another-owner-name"
+			scheme := runtime.NewScheme()
+			utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+			utilruntime.Must(cloudresourcesv1beta1.AddToScheme(scheme))
+			fakeClient := fake.NewClientBuilder().
+				WithScheme(scheme).
+				WithObjects(pv).
+				WithStatusSubresource(pv).
+				Build()
+			k8sClient.(spy.ClientSpy).SetClient(fakeClient)
+
+			err, _ := validatePV(ctx, state)
+
+			assert.NotNilf(t, err, "error should be returned")
+			errorConditions := meta.FindStatusCondition(gcpNfsVolume.Status.Conditions, cloudresourcesv1beta1.ConditionTypeError)
+			assert.NotEmpty(t, errorConditions, "error condition should be added")
+		})
+	})
+}
