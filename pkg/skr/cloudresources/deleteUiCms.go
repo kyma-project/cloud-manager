414d91cff710a3d574c50ec3d3e8dcb39dd47483
Kevin Ho <kevin.ho01@sap.com>
2024-07-09 13:27:22 -0700

    added deleteUiCms.go, uiCm.go, and where to call it.

---
+++ pkg/skr/cloudresources/deleteUiCms.go
@@ -0,0 +1,46 @@
+package cloudresources
+
+import (
+	"context"
+	"github.com/kyma-project/cloud-manager/pkg/composed"
+	"github.com/kyma-project/cloud-manager/pkg/util"
+)
+
+func deleteUiCms(ctx context.Context, st composed.State) (error, context.Context) {
+
+	state := st.(*State)
+	logger := composed.LoggerFromCtx(ctx)
+
+	uiCmList := util.NewUiCmListUnstructured()
+	err := state.Cluster().ApiReader().List(ctx, uiCmList)
+	if err != nil {
+		return composed.LogErrorAndReturn(err, "Error listing UI CMs", composed.StopWithRequeue, ctx)
+	}
+
+	logger.Info("Checking UI ConfigMaps to uninstall")
+
+	// Loop through the uiCmList
+	uiKey := "cloud-manager"
+
+	for _, uiCm := range uiCmList.Items {
+
+		labels := uiCm.GetLabels()
+		value, hasUiKey := labels[uiKey]
+		if hasUiKey && value == "cloud-manager" {
+			// delete
+
+			u := util.NewUiCmUnstructured()
+			u.SetName(uiCm.GetName())
+			// delete call
+			//err = state.Cluster().K8sClient().Delete(ctx, u)
+			//if err != nil {
+			//	return composed.LogErrorAndReturn(err, fmt.Sprintf("Error deleting ConfigMap %s", uiCm.GetName()), composed.StopWithRequeue, ctx)
+			//}
+		} else {
+			continue
+		}
+
+	}
+
+	return nil, nil
+}
--- pkg/skr/cloudresources/reconciler.go
+++ pkg/skr/cloudresources/reconciler.go
@@ -56,6 +56,7 @@ func (r *reconciler) newAction() composed.Action {
 				checkIfResourcesExist,
 				deleteCrds,
 				removeFinalizer,
+				// TODO: call deleteUiCms here
 
 				composed.StopAndForgetAction,
 			),
