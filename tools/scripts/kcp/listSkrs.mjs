3b2a17250f1566f5dbdc5857e6ce68e11b05a2c3
Milos Tomic <milos.tomic@sap.com>
2024-07-09 09:51:58 +0200

    feat(SKR IpRange): change SKR IpRange to cluster scope

--- tools/scripts/kcp/listSkrs.mjs
+++ tools/scripts/kcp/listSkrs.mjs
@@ -2,10 +2,12 @@
 
 const argv = minimist(process.argv.slice(2), {
     alias: {
-        a: 'all',
+        a: "all",
+        o: "output",
     },
     default: {
         all: false,
+        output: "",
     },
     boolean: [
         "all",
@@ -24,33 +26,49 @@ for (let kyma of list.items) {
         globalAccountId: kyma.metadata.labels["kyma-project.io/global-account-id"],
         subaccountId: kyma.metadata.labels["kyma-project.io/subaccount-id"],
         brokerPlanName: kyma.metadata.labels["kyma-project.io/broker-plan-name"],
+        kymaState: kyma.status ? kyma.status.state : "-",
+        id: `${kyma.metadata.name}`,
+        shootName: kyma.metadata.labels["kyma-project.io/shoot-name"],
+        cmFound: false,
+        cmState: "-",
+        cmChannel: "-",
     }
-    let state = kyma.status.state
-    let id = kyma.metadata.name
-    let shoot = kyma.metadata.labels["kyma-project.io/shoot-name"]
-    let cmFound = false
-    let cmState = "-"
-    let cmChannel = "-"
-    if (Array.isArray(kyma.status.modules)) {
+    if (kyma.status && Array.isArray(kyma.status.modules)) {
         for (let m of kyma.status.modules) {
             if (m.name === "cloud-manager") {
-                cmFound = true
-                cmState = m.state
-                cmChannel = m.channel
+                item.cmFound = true
+                item.cmState = m.state
+                item.cmChannel = m.channel
             }
         }
     }
 
-    if (!argv.all && !cmFound) {
+    if (!argv.all && !item.cmFound) {
         continue
     }
 
-    data.push({id, shoot, state, cmState, cmChannel})
+    data.push(item)
 }
 
-console.log(`${"ID".padStart(38)}  ${"SHOOT".padStart(13)} ${"STATE".padStart(12)} ${"CM-STATE".padStart(13)} ${"CM-CHANNEL".padStart(15)}`)
+if (argv.output === "json") {
+    console.log(JSON.stringify(data, undefined, "  "))
+    process.exit();
+}
+
+if (argv.output === "yaml") {
+    console.log(YAML.stringify(data, undefined, "  "))
+    process.exit();
+}
+
+if (argv.output === "wide") {
+    console.log(`${"GLOBAL ACCOUNT".padStart(38)} ${"SUBACCOUNT".padStart(38)} ${"ID".padStart(38)}  ${"SHOOT".padStart(13)} ${"STATE".padStart(12)} ${"CM-STATE".padStart(13)} ${"CM-CHANNEL".padStart(15)}`)
+} else {
+    console.log(`${"ID".padStart(38)}  ${"SHOOT".padStart(13)} ${"STATE".padStart(12)} ${"CM-STATE".padStart(13)} ${"CM-CHANNEL".padStart(15)}`)
+}
 for (let item of data) {
-    let id = item.id
-    let shoot = item.shoot
-    console.log(`${item.id.padStart(38)}  ${item.shoot.padStart(13)} ${item.state.padStart(12)}  ${item.cmState.padStart(13)} ${item.cmChannel.padStart(15)}`)
+    if (argv.output === "wide") {
+        console.log(`${item.globalAccountId.padStart(38)} ${item.subaccountId.padStart(38)} ${item.id.padStart(38)}  ${item.shoot.padStart(13)} ${item.kymaState.padStart(12)}  ${item.cmState.padStart(13)} ${item.cmChannel.padStart(15)}`)
+    } else {
+        console.log(`${item.id.padStart(38)}  ${item.shoot.padStart(13)} ${item.kymaState.padStart(12)}  ${item.cmState.padStart(13)} ${item.cmChannel.padStart(15)}`)
+    }
 }
