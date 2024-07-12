137144beca234221a0aa7f02b95fea667d76317b
Ivan Jovovic <ivan.jovovic@sap.com>
2024-07-12 14:22:30 +0200

    revert port changes

--- cmd/main.go
+++ cmd/main.go
@@ -93,8 +93,8 @@ func main() {
 	var enableLeaderElection bool
 	var probeAddr string
 	var gcpStructuredLogging bool
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8085", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8084", "The address the probe endpoint binds to.")
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
