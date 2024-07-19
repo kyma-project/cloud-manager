e3fa7d406f8ae128646a1ef9fd789707e2f61685
Milos Tomic <59831542+tmilos77@users.noreply.github.com>
2024-07-19 17:06:38 +0200

    feat: CC Scope (#361)

--- api/cloud-control/v1beta1/providerType.go
+++ api/cloud-control/v1beta1/providerType.go
@@ -3,7 +3,8 @@ package v1beta1
 type ProviderType string
 
 const (
-	ProviderGCP   = ProviderType("gcp")
-	ProviderAzure = ProviderType("azure")
-	ProviderAws   = ProviderType("aws")
+	ProviderGCP       = ProviderType("gcp")
+	ProviderAzure     = ProviderType("azure")
+	ProviderAws       = ProviderType("aws")
+	ProviderOpenStack = ProviderType("openstack")
 )
