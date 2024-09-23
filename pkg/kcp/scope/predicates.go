0577c0a46a7001d6dbb27189c3e43d30fb971191
Milos Tomic <milos.tomic@sap.com>
2024-09-23 15:37:56 +0200

    chore: scope network fix

--- pkg/kcp/scope/predicates.go
+++ pkg/kcp/scope/predicates.go
@@ -10,9 +10,20 @@ func predicateShouldEnable() composed.Predicate {
 	return func(ctx context.Context, st composed.State) bool {
 		state := st.(*State)
 
-		// module is listed in Kyma status, but SKR is not active
-		if state.moduleState != util.KymaModuleStateNotPresent && !state.skrActive {
-			return true
+		// module is listed in Kyma status
+		if state.moduleState != util.KymaModuleStateNotPresent {
+			// but SKR is not active
+			if !state.skrActive {
+				return true
+			}
+			// or scope does not exist
+			if !ObjIsLoadedPredicate()(ctx, state) {
+				return true
+			}
+			// or kyma network does not exist
+			if state.kcpNetworkKyma == nil {
+				return true
+			}
 		}
 
 		return false
