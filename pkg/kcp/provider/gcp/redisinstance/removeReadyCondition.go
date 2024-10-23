d0444941d45ea36a767e25efe498017d4bf1a316
Dušan Pantić <dushanpantic@users.noreply.github.com>
2024-10-23 10:44:26 +0200

    chore: print serialized object (#756)

--- pkg/kcp/provider/gcp/redisinstance/removeReadyCondition.go
+++ pkg/kcp/provider/gcp/redisinstance/removeReadyCondition.go
@@ -2,6 +2,7 @@ package redisinstance
 
 import (
 	"context"
+	"encoding/json"
 
 	cloudcontrolv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-control/v1beta1"
 	"github.com/kyma-project/cloud-manager/pkg/composed"
@@ -22,7 +23,16 @@ func removeReadyCondition(ctx context.Context, st composed.State) (error, contex
 	logger.Info("Removing Ready condition")
 
 	meta.RemoveStatusCondition(redisInstance.Conditions(), cloudcontrolv1beta1.ConditionTypeReady)
-	err := state.UpdateObjStatus(ctx)
+
+	serializedJson, err := json.Marshal(redisInstance.CloneForPatchStatus())
+	if err != nil {
+		logger.Info("Failed to serialize to json")
+	}
+	if serializedJson != nil {
+		logger.WithValues("serializedJson", string(serializedJson)).Info("serialized json")
+	}
+
+	err = state.UpdateObjStatus(ctx)
 	if err != nil {
 		return composed.LogErrorAndReturn(err, "Error updating RedisInstance status after removing Ready condition", composed.StopWithRequeue, ctx)
 	}
