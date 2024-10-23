775ca61b65bee127535304dc9e95fd3a2c5c5094
Dušan <dusan.pantic@sap.com>
2024-10-23 11:26:34 +0200

    Revert "chore: print serialized object (#756)"
    This reverts commit d0444941d45ea36a767e25efe498017d4bf1a316.

--- pkg/kcp/provider/gcp/redisinstance/removeReadyCondition.go
+++ pkg/kcp/provider/gcp/redisinstance/removeReadyCondition.go
@@ -2,7 +2,6 @@ package redisinstance
 
 import (
 	"context"
-	"encoding/json"
 
 	cloudcontrolv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-control/v1beta1"
 	"github.com/kyma-project/cloud-manager/pkg/composed"
@@ -23,16 +22,7 @@ func removeReadyCondition(ctx context.Context, st composed.State) (error, contex
 	logger.Info("Removing Ready condition")
 
 	meta.RemoveStatusCondition(redisInstance.Conditions(), cloudcontrolv1beta1.ConditionTypeReady)
-
-	serializedJson, err := json.Marshal(redisInstance.CloneForPatchStatus())
-	if err != nil {
-		logger.Info("Failed to serialize to json")
-	}
-	if serializedJson != nil {
-		logger.WithValues("serializedJson", string(serializedJson)).Info("serialized json")
-	}
-
-	err = state.UpdateObjStatus(ctx)
+	err := state.UpdateObjStatus(ctx)
 	if err != nil {
 		return composed.LogErrorAndReturn(err, "Error updating RedisInstance status after removing Ready condition", composed.StopWithRequeue, ctx)
 	}
