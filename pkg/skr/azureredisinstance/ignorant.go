fe49b255ced5ab992fbf04f78173495645e406a0
Ivan Jovovic <ivan.jovovic@sap.com>
2024-10-29 12:05:54 +0100

    tests: prevent deleting ip range while in use

---
+++ pkg/skr/azureredisinstance/ignorant.go
@@ -0,0 +1,5 @@
+package azureredisinstance
+
+import "github.com/kyma-project/cloud-manager/pkg/common/ignorant"
+
+var Ignore = ignorant.New()
--- pkg/testinfra/dsl/commonIpRange.go
+++ pkg/testinfra/dsl/commonIpRange.go
@@ -36,6 +36,12 @@ func WithIpRange(ipRangeName string) ObjAction {
 				}
 				return
 			}
+			if x, ok := obj.(*cloudresourcesv1beta1.AzureRedisInstance); ok {
+				if x.Spec.IpRange.Name == "" {
+					x.Spec.IpRange.Name = ipRangeName
+				}
+				return
+			}
 
 			// KCP
 			if x, ok := obj.(*cloudcontrolv1beta1.NfsInstance); ok {
