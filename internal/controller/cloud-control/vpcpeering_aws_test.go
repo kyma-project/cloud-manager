b6b937d9e662720b130b0b0f78999db5542ad843
Vladimir Andjelkoski <vladimir.andjelkoski@sap.com>
2024-12-06 12:08:06 +0100

    Update internal/controller/cloud-control/vpcpeering_aws_test.go
    Co-authored-by: Dušan Pantić <dushanpantic@users.noreply.github.com>

--- internal/controller/cloud-control/vpcpeering_aws_test.go
+++ internal/controller/cloud-control/vpcpeering_aws_test.go
@@ -750,7 +750,7 @@ var _ = Describe("Feature: KCP VpcPeering", func() {
 				Should(Succeed())
 		})
 
-		By("And remote KCP Network is Ready", func() {
+		By("And Given remote KCP Network is Ready", func() {
 			Eventually(LoadAndCheck).
 				WithArguments(infra.Ctx(), infra.KCP().Client(), localKcpNet,
 					NewObjActions(),
