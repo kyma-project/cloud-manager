df75b80917fbe636817c88e387caf384c4c38f5c
Dušan Pantić <dushanpantic@users.noreply.github.com>
2024-12-12 13:06:17 +0100

    refactor(RedisInstance): move auth secret to separate file (#886)

---
+++ api/cloud-resources/v1beta1/redisinstance_authsecret.go
@@ -0,0 +1,7 @@
+package v1beta1
+
+type AuthSecretSpec struct {
+	Name        string            `json:"name,omitempty"`
+	Labels      map[string]string `json:"labels,omitempty"`
+	Annotations map[string]string `json:"annotations,omitempty"`
+}
