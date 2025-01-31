9057d09b6e2dc3bdaba74dfe2fef19ea53822f9b
Dušan <dusan.pantic@sap.com>
2025-01-31 13:38:17 +0100

    feat(RedisCluster): add cloud-control boilerplate

---
+++ internal/controller/cloud-control/rediscluster_controller.go
@@ -0,0 +1,82 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cloudcontrol
+
+import (
+	"context"
+
+	cloudcontrolv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-control/v1beta1"
+	"github.com/kyma-project/cloud-manager/pkg/common/abstractions"
+	"github.com/kyma-project/cloud-manager/pkg/common/actions/focal"
+	"github.com/kyma-project/cloud-manager/pkg/composed"
+	"github.com/kyma-project/cloud-manager/pkg/kcp/rediscluster"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+)
+
+func SetupRedisClusterReconciler(
+	kcpManager manager.Manager,
+	env abstractions.Environment,
+) error {
+	if env == nil {
+		env = abstractions.NewOSEnvironment()
+	}
+	return NewRedisClusterReconciler(
+		rediscluster.NewRedisClusterReconciler(
+			composed.NewStateFactory(composed.NewStateClusterFromCluster(kcpManager)),
+			focal.NewStateFactory(),
+		),
+	).SetupWithManager(kcpManager)
+}
+
+func NewRedisClusterReconciler(
+	reconciler rediscluster.RedisClusterReconciler,
+) *RedisClusterReconciler {
+	return &RedisClusterReconciler{
+		Reconciler: reconciler,
+	}
+}
+
+type RedisClusterReconciler struct {
+	Reconciler rediscluster.RedisClusterReconciler
+}
+
+// +kubebuilder:rbac:groups=cloud-control.kyma-project.io,resources=redisclusters,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=cloud-control.kyma-project.io,resources=redisclusters/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=cloud-control.kyma-project.io,resources=redisclusters/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the RedisCluster object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.0/pkg/reconcile
+func (r *RedisClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	return r.Reconciler.Reconcile(ctx, req)
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *RedisClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&cloudcontrolv1beta1.RedisCluster{}, builder.WithPredicates(predicate.ResourceVersionChangedPredicate{})).
+		Complete(r)
+}
