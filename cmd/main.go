299ebd7d8497ed44691243fcb457f74c3303426c
Dušan <dusan.pantic@sap.com>
2025-02-07 11:01:46 +0100

    refactor(RedisInstance): move aws client to common package

--- cmd/main.go
+++ cmd/main.go
@@ -39,10 +39,10 @@ import (
 	"github.com/kyma-project/cloud-manager/pkg/quota"
 
 	"github.com/kyma-project/cloud-manager/pkg/common/abstractions"
+	awsclient "github.com/kyma-project/cloud-manager/pkg/kcp/provider/aws/client"
 	awsiprangeclient "github.com/kyma-project/cloud-manager/pkg/kcp/provider/aws/iprange/client"
 	awsnfsinstanceclient "github.com/kyma-project/cloud-manager/pkg/kcp/provider/aws/nfsinstance/client"
 	awsnukenfsclient "github.com/kyma-project/cloud-manager/pkg/kcp/provider/aws/nuke/client"
-	awsredisinstanceclient "github.com/kyma-project/cloud-manager/pkg/kcp/provider/aws/redisinstance/client"
 	awsvpcpeeringclient "github.com/kyma-project/cloud-manager/pkg/kcp/provider/aws/vpcpeering/client"
 	azureiprangeclient "github.com/kyma-project/cloud-manager/pkg/kcp/provider/azure/iprange/client"
 	azurenetworkclient "github.com/kyma-project/cloud-manager/pkg/kcp/provider/azure/network/client"
@@ -303,7 +303,7 @@ func main() {
 		mgr,
 		gcpmemorystoreclient.NewMemorystoreClientProvider(),
 		azureredisclient.NewClientProvider(),
-		awsredisinstanceclient.NewClientProvider(),
+		awsclient.NewElastiCacheClientProvider(),
 		env,
 	); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "RedisInstance")
--- pkg/kcp/provider/aws/redisinstance/createElastiCacheCluster.go
+++ pkg/kcp/provider/aws/redisinstance/createElastiCacheCluster.go
@@ -8,7 +8,7 @@ import (
 	cloudcontrolv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-control/v1beta1"
 	"github.com/kyma-project/cloud-manager/pkg/common"
 	"github.com/kyma-project/cloud-manager/pkg/composed"
-	"github.com/kyma-project/cloud-manager/pkg/kcp/provider/aws/redisinstance/client"
+	"github.com/kyma-project/cloud-manager/pkg/kcp/provider/aws/client"
 	"github.com/kyma-project/cloud-manager/pkg/util"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
