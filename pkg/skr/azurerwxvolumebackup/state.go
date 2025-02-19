29ac010a8053047f0aaa040dbc84a4bb7df8536f
Kevin Ho <kevin.ho01@sap.com>
2025-02-19 15:46:21 -0800

    first pass - AzureRwxVolumeBackup CRD

---
+++ pkg/skr/azurerwxvolumebackup/state.go
@@ -0,0 +1,43 @@
+package azurerwxvolumebackup
+
+import (
+	cloudresourcesv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-resources/v1beta1"
+	"github.com/kyma-project/cloud-manager/pkg/composed"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/klog/v2"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+type State struct {
+	composed.State
+	KymaRef    klog.ObjectRef
+	KcpCluster composed.StateCluster
+
+	AuthSecret *corev1.Secret
+}
+
+type stateFactory struct {
+	baseStateFactory composed.StateFactory
+	kymaRef          klog.ObjectRef
+	kcpCluster       composed.StateCluster
+}
+
+func (f *stateFactory) NewState(req ctrl.Request) *State {
+	return &State{
+		State:      f.baseStateFactory.NewState(req.NamespacedName, &cloudresourcesv1beta1.AzureRedisInstance{}),
+		KymaRef:    f.kymaRef,
+		KcpCluster: f.kcpCluster,
+	}
+}
+
+func newStateFactory(
+	baseStateFactory composed.StateFactory,
+	kymaRef klog.ObjectRef,
+	kcpCluster composed.StateCluster,
+) *stateFactory {
+	return &stateFactory{
+		baseStateFactory: baseStateFactory,
+		kymaRef:          kymaRef,
+		kcpCluster:       kcpCluster,
+	}
+}
