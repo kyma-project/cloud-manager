621f8d28264505b49fb6606d9b60c26faa38bfb7
kevin-kho <kevin.ho01@sap.com>
2025-02-25 12:04:06 -0800

    feat: Azure Client Implementation (#1053)

--- pkg/skr/azurerwxvolumebackup/client/protectionPolicies.go
+++ pkg/skr/azurerwxvolumebackup/client/protectionPolicies.go
@@ -2,17 +2,19 @@ package client
 
 import (
 	"context"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/recoveryservices/armrecoveryservicesbackup/v4"
+	"time"
 )
 
 type ProtectionPoliciesClient interface {
-	CreateBackupPolicy(ctx context.Context)
-	DeleteBackupPolicy(ctx context.Context)
+	CreateBackupPolicy(ctx context.Context, vaultName string, resourceGroupName string, policyName string) error
+	DeleteBackupPolicy(ctx context.Context, vaultName string, resourceGroupName string, policyName string) error
 }
 
 type protectionPoliciesClient struct {
-	*armrecoveryservicesbackup.ProtectionPoliciesClient
+	azureClient *armrecoveryservicesbackup.ProtectionPoliciesClient
 }
 
 func NewProtectionPoliciesClient(subscriptionId string, cred *azidentity.ClientSecretCredential) (ProtectionPoliciesClient, error) {
@@ -26,10 +28,60 @@ func NewProtectionPoliciesClient(subscriptionId string, cred *azidentity.ClientS
 	return protectionPoliciesClient{ppc}, nil
 }
 
-func (c protectionPoliciesClient) CreateBackupPolicy(ctx context.Context) {
-	// TODO: implementation details
+func (c protectionPoliciesClient) CreateBackupPolicy(ctx context.Context, vaultName string, resourceGroupName string, policyName string) error {
+
+	ppr := armrecoveryservicesbackup.ProtectionPolicyResource{
+		ETag:     nil,
+		Location: nil,
+		Properties: to.Ptr(armrecoveryservicesbackup.AzureFileShareProtectionPolicy{
+			BackupManagementType:           to.Ptr("AzureStorage"),
+			ProtectedItemsCount:            nil,
+			ResourceGuardOperationRequests: nil,
+			RetentionPolicy: to.Ptr(armrecoveryservicesbackup.LongTermRetentionPolicy{
+				RetentionPolicyType: to.Ptr("LongTermRetentionPolicy"),
+				DailySchedule: to.Ptr(armrecoveryservicesbackup.DailyRetentionSchedule{
+					RetentionDuration: to.Ptr(armrecoveryservicesbackup.RetentionDuration{
+						Count:        to.Ptr(int32(30)),
+						DurationType: to.Ptr(armrecoveryservicesbackup.RetentionDurationTypeDays),
+					}),
+					RetentionTimes: []*time.Time{to.Ptr(time.Now())},
+				}),
+				MonthlySchedule: nil,
+				WeeklySchedule:  nil,
+				YearlySchedule:  nil,
+			}),
+			SchedulePolicy: to.Ptr(armrecoveryservicesbackup.SimpleSchedulePolicy{
+				SchedulePolicyType:      nil,
+				HourlySchedule:          nil,
+				ScheduleRunDays:         nil,
+				ScheduleRunFrequency:    to.Ptr(armrecoveryservicesbackup.ScheduleRunTypeDaily),
+				ScheduleRunTimes:        []*time.Time{to.Ptr(time.Now())},
+				ScheduleWeeklyFrequency: nil,
+			}),
+			TimeZone:             to.Ptr("UTC"),
+			VaultRetentionPolicy: nil,
+			WorkLoadType:         to.Ptr(armrecoveryservicesbackup.WorkloadTypeAzureFileShare),
+		}),
+		Tags: map[string]*string{"cloud-manager": to.Ptr("rwxVolumeBackup")},
+		ID:   nil,
+		Name: nil,
+		Type: nil,
+	}
+
+	_, err := c.azureClient.CreateOrUpdate(ctx, vaultName, resourceGroupName, policyName, ppr, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+
 }
 
-func (c protectionPoliciesClient) DeleteBackupPolicy(ctx context.Context) {
-	// TODO: implementation details
+func (c protectionPoliciesClient) DeleteBackupPolicy(ctx context.Context, vaultName string, resourceGroupName string, policyName string) error {
+
+	_, err := c.azureClient.BeginDelete(ctx, vaultName, resourceGroupName, policyName, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- pkg/skr/azurerwxvolumebackup/client/vault.go
+++ pkg/skr/azurerwxvolumebackup/client/vault.go
@@ -2,17 +2,21 @@ package client
 
 import (
 	"context"
+	"fmt"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/recoveryservices/armrecoveryservices"
+	"slices"
 )
 
 type VaultClient interface {
-	CreateVault(ctx context.Context)
-	DeleteVault(ctx context.Context)
+	CreateVault(ctx context.Context, resourceGroupName string, vaultName string, location string) error
+	DeleteVault(ctx context.Context, resourceGroupName string, vaultName string) error
+	ListVaults(ctx context.Context) ([]*armrecoveryservices.Vault, error)
 }
 
 type vaultClient struct {
-	*armrecoveryservices.VaultsClient
+	azureClient *armrecoveryservices.VaultsClient
 }
 
 func NewVaultClient(subscriptionId string, cred *azidentity.ClientSecretCredential) (VaultClient, error) {
@@ -25,10 +29,94 @@ func NewVaultClient(subscriptionId string, cred *azidentity.ClientSecretCredenti
 	return vaultClient{vc}, nil
 }
 
-func (c vaultClient) CreateVault(ctx context.Context) {
-	// TODO: implementation details
+func (c vaultClient) vaultExists(ctx context.Context, location string) (bool, error) {
+	vaults, err := c.ListVaults(ctx)
+	if err != nil {
+		return false, err
+	}
+
+	slices.ContainsFunc(vaults, func(vault *armrecoveryservices.Vault) bool {
+
+		if vault.Location == nil || vault.Tags == nil {
+			return false
+		}
+
+		_, tagExists := vault.Tags["cloud-manager"]
+
+		return *vault.Location == location && tagExists
+
+	})
+	return false, nil
 }
 
-func (c vaultClient) DeleteVault(ctx context.Context) {
-	// TODO: implementation details
+func (c vaultClient) CreateVault(ctx context.Context, resourceGroupName string, vaultName string, location string) error {
+
+	// Fail if vault exists
+	exists, err := c.vaultExists(ctx, location)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return fmt.Errorf("vault already exists in %s", location)
+	}
+
+	_, err = c.azureClient.BeginCreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		vaultName,
+		armrecoveryservices.Vault{
+			Location: to.Ptr(location),
+			Properties: to.Ptr(armrecoveryservices.VaultProperties{
+				PublicNetworkAccess: to.Ptr(armrecoveryservices.PublicNetworkAccessEnabled),
+			}),
+			SKU: to.Ptr(armrecoveryservices.SKU{
+				Name: to.Ptr(armrecoveryservices.SKUNameStandard),
+			}),
+			Tags: map[string]*string{"cloud-manager": to.Ptr("rwxVolumeBackup")},
+		},
+		nil,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func (c vaultClient) DeleteVault(ctx context.Context, resourceGroupName string, vaultName string) error {
+
+	_, err := c.azureClient.Delete(
+		ctx,
+		resourceGroupName,
+		vaultName,
+		to.Ptr(armrecoveryservices.VaultsClientDeleteOptions{}),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c vaultClient) ListVaults(ctx context.Context) ([]*armrecoveryservices.Vault, error) {
+
+	pager := c.azureClient.NewListBySubscriptionIDPager(
+		&armrecoveryservices.VaultsClientListBySubscriptionIDOptions{},
+	)
+
+	var vaults []*armrecoveryservices.Vault
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
+		if err != nil {
+			return vaults, err
+		}
+
+		vaults = append(vaults, page.VaultList.Value...)
+
+	}
+	return vaults, nil
+
 }
