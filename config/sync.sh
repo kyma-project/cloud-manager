de81a860bf18b6743013ae7024710784cf14dc7a
ravi-shankar-sap <146867021+ravi-shankar-sap@users.noreply.github.com>
2025-02-27 11:53:40 -0800

    feat: AzureRwxBackupSchedule CRD (#1056)

--- config/sync.sh
+++ config/sync.sh
@@ -56,6 +56,7 @@ cp $SCRIPT_DIR/crd/bases/cloud-resources.kyma-project.io_azureredisinstances.yam
 cp $SCRIPT_DIR/crd/bases/cloud-resources.kyma-project.io_ipranges.yaml      $SCRIPT_DIR/dist/skr/crd/bases/providers/azure/
 cp $SCRIPT_DIR/crd/bases/cloud-resources.kyma-project.io_azurerwxvolumebackups.yaml    $SCRIPT_DIR/dist/skr/crd/bases/providers/azure/
 cp $SCRIPT_DIR/crd/bases/cloud-resources.kyma-project.io_azurerwxvolumerestores.yaml    $SCRIPT_DIR/dist/skr/crd/bases/providers/azure/
+cp $SCRIPT_DIR/crd/bases/cloud-resources.kyma-project.io_azurerwxbackupschedules.yaml    $SCRIPT_DIR/dist/skr/crd/bases/providers/azure/
 
 # AZURE UI
 cp $SCRIPT_DIR/ui-extensions/azurevpcpeerings/cloud-resources.kyma-project.io_azurevpcpeerings_ui.yaml $SCRIPT_DIR/dist/skr/crd/bases/providers/azure
