b1d2ea269a47422b825fe46c0b2dfe748876f50b
Amir Balaie <amir.balaie@sap.com>
2025-03-12 10:18:27 -0700

    feat: AzureRwxVolumeRestore reconciler

--- pkg/skr/azurerwxvolumebackup/client/constants.go
+++ pkg/skr/azurerwxvolumebackup/client/constants.go
@@ -1,6 +1,34 @@
 package client
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
+
+type pvVolumeHandleRegexGroupKey string
+
+const (
+	vhStorageAccountName pvVolumeHandleRegexGroupKey = "storageAccountName"
+	vhResourceGroupName  pvVolumeHandleRegexGroupKey = "resourceGroupName"
+	vhFileShareName      pvVolumeHandleRegexGroupKey = "fileShareName"
+	vhUuid               pvVolumeHandleRegexGroupKey = "uuid"
+	vhSecretNamespace    pvVolumeHandleRegexGroupKey = "secretNamespace"
+)
+
+type recoverPointIdRegexGroupKey string
+
+const (
+	rpSubscription    recoverPointIdRegexGroupKey = "subscription"
+	rpResourceGroup   recoverPointIdRegexGroupKey = "resourceGroup"
+	rpVault           recoverPointIdRegexGroupKey = "vault"
+	rpContainer       recoverPointIdRegexGroupKey = "container"
+	rpProtectedItem   recoverPointIdRegexGroupKey = "protectedItem"
+	rpRecoveryPointId recoverPointIdRegexGroupKey = "recoveryPointId"
+)
+
+const pvVolumeHandlePattern = "(?<resourceGroupName>[^\\#]*)#(?<storageAccountName>[^\\#]*)#(?<fileShareName>[^\\#]*)#(?<placeHolder>[^\\#]*)#(?<uuid>[^\\#]*)#(?<secretNamespace>[^\\#]+)"
+
+const recoverPointIdPattern = "\\/subscriptions\\/(?<subscription>[^\\/]*)\\/resourceGroups\\/(?<resourceGroup>[^\\/]*)\\/providers\\/Microsoft.RecoveryServices\\/vaults\\/(?<vault>[^\\/]*)\\/backupFabrics\\/Azure\\/protectionContainers\\/(?<container>[^\\/]*)\\/protectedItems\\/(?<protectedItem>[^\\/]*)\\/recoveryPoints\\/(?<recoveryPointId>[^\\/]*)"
 
 const (
 	storageAccountPathPattern = "/subscriptions/%v/resourceGroups/%v/providers/Microsoft.Storage/storageAccounts/%v"
@@ -9,6 +37,11 @@ const (
 	containerNamePattern      = "StorageContainer;Storage;%v;%v"
 	recoveryPointPathPattern  = "/subscriptions/%v/resourceGroups/%v/providers/Microsoft.RecoveryServices/vaults/%v/backupFabrics/Azure/protectionContainers/%v/protectedItems/%v/recoveryPoints/%v"
 )
+
+const storageProvisionerKey = "volume.kubernetes.io/storage-provisioner"
+const azureFileShareProvisioner = "file.csi.azure.com"
+
+const AzureFabricName = "Azure"
 
 func GetStorageAccountPath(subscriptionId, resourceGroupName, storageAccountName string) string {
 	return fmt.Sprintf(storageAccountPathPattern, subscriptionId, resourceGroupName, storageAccountName)
@@ -29,4 +62,38 @@ func GetContainerName(resourceGroupName, storageAccountName string) string {
 func GetRecoveryPointPath(subscriptionId, resourceGroupName, vaultName, storageAccountName, protectedItemName, recoveryPointName string) string {
 	containerName := GetContainerName(resourceGroupName, storageAccountName)
 	return fmt.Sprintf(recoveryPointPathPattern, subscriptionId, resourceGroupName, vaultName, containerName, protectedItemName, recoveryPointName)
+}
+func ParsePvVolumeHandle(pvVolumeHandle string) (resourceGroupName string, storageAccountName string, fileShareName string, uuid string, secretNamespace string, err error) {
+	re := regexp.MustCompile(pvVolumeHandlePattern)
+	match := re.FindStringSubmatch(pvVolumeHandle)
+	if match == nil {
+		return "", "", "", "", "", fmt.Errorf("pvVolumeHandle %s does not match pattern %s", pvVolumeHandle, pvVolumeHandlePattern)
+	}
+	result := make(map[pvVolumeHandleRegexGroupKey]string)
+	for i, name := range re.SubexpNames() {
+		if i != 0 && name != "" {
+			result[pvVolumeHandleRegexGroupKey(name)] = match[i]
+		}
+	}
+	return result[vhResourceGroupName], result[vhStorageAccountName], result[vhFileShareName], result[vhUuid], result[vhSecretNamespace], nil
+}
+
+func ParseRecoveryPointId(recoveryPointId string) (subscription string, resourceGroup string, vault string, container string, protectedItem string, recoveryPointIdValue string, err error) {
+	re := regexp.MustCompile(recoverPointIdPattern)
+	match := re.FindStringSubmatch(recoveryPointId)
+	if match == nil {
+		return "", "", "", "", "", "", fmt.Errorf("recoveryPointId %s does not match pattern %s", recoveryPointId, recoverPointIdPattern)
+	}
+	result := make(map[recoverPointIdRegexGroupKey]string)
+	for i, name := range re.SubexpNames() {
+		if i != 0 && name != "" {
+			result[recoverPointIdRegexGroupKey(name)] = match[i]
+		}
+	}
+	return result[rpSubscription], result[rpResourceGroup], result[rpVault], result[rpContainer], result[rpProtectedItem], result[rpRecoveryPointId], nil
+}
+
+func IsPvcProvisionerAzureCsiDriver(annotations map[string]string) bool {
+	provisioner, ok := annotations[storageProvisionerKey]
+	return ok && provisioner == azureFileShareProvisioner
 }
---
+++ pkg/skr/azurerwxvolumerestore/loadPersistentVolumeClaim_test.go
@@ -0,0 +1,149 @@
+package azurerwxvolumerestore
+
+import (
+	"context"
+	"testing"
+
+	cloudresourcesv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-resources/v1beta1"
+	"github.com/kyma-project/cloud-manager/pkg/composed"
+	spy "github.com/kyma-project/cloud-manager/pkg/testinfra/clientspy"
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+func TestLoadPersistentVolumeClaim(t *testing.T) {
+
+	t.Run("loadPersistentVolumeClaim", func(t *testing.T) {
+
+		var azureRwxVolumeRestore *cloudresourcesv1beta1.AzureRwxVolumeRestore
+		var pvc *corev1.PersistentVolumeClaim
+		var state *State
+		var k8sClient client.WithWatch
+
+		createEmptyState := func(k8sClient client.WithWatch, azureRwxVolumeRestore *cloudresourcesv1beta1.AzureRwxVolumeRestore) *State {
+			cluster := composed.NewStateCluster(k8sClient, k8sClient, nil, k8sClient.Scheme())
+			return &State{
+				State: composed.NewStateFactory(cluster).NewState(types.NamespacedName{}, azureRwxVolumeRestore),
+			}
+		}
+
+		setupTest := func(withPvc bool, pvcStatus corev1.PersistentVolumeClaimPhase) {
+			azureRwxVolumeRestore = &cloudresourcesv1beta1.AzureRwxVolumeRestore{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test-azure-restore",
+					Namespace: "test-ns-2",
+				},
+				Spec: cloudresourcesv1beta1.AzureRwxVolumeRestoreSpec{
+
+					Destination: cloudresourcesv1beta1.PvcSource{
+						Pvc: cloudresourcesv1beta1.PvcRef{
+							Name:      "test-azure-restore-pvc",
+							Namespace: "test-ns",
+						},
+					},
+					Source: cloudresourcesv1beta1.AzureRwxVolumeRestoreSource{
+						Backup: cloudresourcesv1beta1.AzureRwxVolumeBackupRef{
+							Name:      "test-azure-restore-backup",
+							Namespace: "test-ns",
+						},
+					},
+				},
+			}
+
+			scheme := runtime.NewScheme()
+			utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+			utilruntime.Must(cloudresourcesv1beta1.AddToScheme(scheme))
+			var fakeClient client.WithWatch
+			if withPvc {
+				pvc = &corev1.PersistentVolumeClaim{
+					ObjectMeta: v1.ObjectMeta{
+						Name:      "test-azure-restore-pvc",
+						Namespace: "test-ns",
+						Annotations: map[string]string{
+							"volume.kubernetes.io/storage-provisioner": "file.csi.azure.com",
+						},
+					},
+					Status: corev1.PersistentVolumeClaimStatus{
+						Phase: pvcStatus,
+					},
+				}
+				fakeClient = fake.NewClientBuilder().
+					WithScheme(scheme).
+					WithObjects(azureRwxVolumeRestore).
+					WithStatusSubresource(azureRwxVolumeRestore).
+					WithObjects(pvc).
+					WithStatusSubresource(pvc).
+					Build()
+			} else {
+				fakeClient = fake.NewClientBuilder().
+					WithScheme(scheme).
+					WithObjects(azureRwxVolumeRestore).
+					WithStatusSubresource(azureRwxVolumeRestore).
+					Build()
+			}
+			k8sClient = spy.NewClientSpy(fakeClient)
+
+			state = createEmptyState(k8sClient, azureRwxVolumeRestore)
+		}
+
+		t.Run("Should: load Bound pvc", func(t *testing.T) {
+			setupTest(true, corev1.ClaimBound)
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			err, res := loadPersistentVolumeClaim(ctx, state)
+
+			assert.Nil(t, res, "should return nil res")
+			assert.Nil(t, err, "should return nil err")
+			assert.NotNilf(t, state.pvc, "pvc should be loaded into state")
+			assert.Equal(t, state.pvc, pvc, "loaded pvc should be the same as the one in the test")
+		})
+
+		t.Run("Should: fail pvc that is not Bound", func(t *testing.T) {
+			setupTest(true, corev1.ClaimPending)
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			err, res := loadPersistentVolumeClaim(ctx, state)
+
+			assert.Equal(t, ctx, res, "should return same context")
+			assert.Equal(t, err, composed.StopAndForget, "should stop and forget")
+			assert.Nil(t, state.pvc, "pvc should remain nil in state")
+		})
+
+		t.Run("Should: fail pvc with invalid provisioner", func(t *testing.T) {
+			setupTest(true, corev1.ClaimPending)
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			// patch the annotations to have invalid provisioner
+			pvc.Annotations["volume.kubernetes.io/storage-provisioner"] = "invalid.provisioner"
+			err := k8sClient.Patch(ctx, pvc, client.MergeFrom(pvc), client.FieldOwner("test"))
+			assert.Nil(t, err, "should patch pvc with invalid provisioner")
+			err, res := loadPersistentVolumeClaim(ctx, state)
+
+			assert.Equal(t, ctx, res, "should return same context")
+			assert.Equal(t, err, composed.StopAndForget, "should stop and forget")
+			assert.Nil(t, state.pvc, "pvc should remain nil in state")
+		})
+
+		t.Run("Should: error out if APIServer cant find requested pvc", func(t *testing.T) {
+			setupTest(false, corev1.ClaimPending)
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			err, res := loadPersistentVolumeClaim(ctx, state)
+
+			assert.Equal(t, ctx, res, "should return same context")
+			assert.Equal(t, err, composed.StopAndForget, "should stop and forget")
+			assert.Nil(t, state.pvc, "pvc should remain nil in state as it is not found in APIServer")
+		})
+
+	})
+}
