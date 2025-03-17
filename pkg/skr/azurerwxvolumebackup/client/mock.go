8cd4a99e1ceac42e9f3f3b503a31b65491397d08
Vladimir Andjelkoski <vladimir.andjelkoski@sap.com>
2025-03-17 11:06:25 +0100

    feat(AzureVpcPeering): cross tenant support

--- pkg/skr/azurerwxvolumebackup/client/mock.go
+++ pkg/skr/azurerwxvolumebackup/client/mock.go
@@ -6,7 +6,7 @@ import (
 )
 
 func NewMockClient() azureclient.ClientProvider[Client] {
-	return func(ctx context.Context, _, _, _, _ string) (Client, error) {
+	return func(ctx context.Context, _, _, _, _ string, _ ...string) (Client, error) {
 		jobsMock = &jobsMockClient{
 			jobsClient: *newJobsMockClient(),
 		}
