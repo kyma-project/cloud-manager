5221ec1f2fb007a054c73246dc536fa2c61da68b
kevin-kho <kevin.ho01@sap.com>
2025-03-19 13:19:41 -0700

    feat:  AzureRwxBackup (#1100)

--- pkg/skr/azurerwxvolumebackup/state.go
+++ pkg/skr/azurerwxvolumebackup/state.go
@@ -1,9 +1,13 @@
 package azurerwxvolumebackup
 
 import (
+	cloudcontrolv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-control/v1beta1"
 	cloudresourcesv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-resources/v1beta1"
 	"github.com/kyma-project/cloud-manager/pkg/composed"
+	client2 "github.com/kyma-project/cloud-manager/pkg/kcp/provider/azure/client"
+	"github.com/kyma-project/cloud-manager/pkg/skr/azurerwxvolumebackup/client"
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
@@ -12,21 +16,39 @@ type State struct {
 	composed.State
 	KymaRef    klog.ObjectRef
 	KcpCluster composed.StateCluster
+	SkrCluster composed.StateCluster
 
-	AuthSecret *corev1.Secret
+	AuthSecret         *corev1.Secret
+	client             client.Client
+	clientProvider     client2.ClientProvider[client.Client]
+	resourceGroupName  string
+	storageAccountName string
+	fileShareName      string
+	pvc                *v1.PersistentVolumeClaim
+	vaultName          string
+	scope              *cloudcontrolv1beta1.Scope
+}
+
+func (s *State) ObjAsAzureRwxVolumeBackup() *cloudresourcesv1beta1.AzureRwxVolumeBackup {
+	return s.Obj().(*cloudresourcesv1beta1.AzureRwxVolumeBackup)
 }
 
 type stateFactory struct {
 	baseStateFactory composed.StateFactory
 	kymaRef          klog.ObjectRef
 	kcpCluster       composed.StateCluster
+	skrCluster       composed.StateCluster
+	clientProvider   client2.ClientProvider[client.Client]
 }
 
 func (f *stateFactory) NewState(req ctrl.Request) *State {
+
 	return &State{
-		State:      f.baseStateFactory.NewState(req.NamespacedName, &cloudresourcesv1beta1.AzureRwxVolumeBackup{}),
-		KymaRef:    f.kymaRef,
-		KcpCluster: f.kcpCluster,
+		State:          f.baseStateFactory.NewState(req.NamespacedName, &cloudresourcesv1beta1.AzureRwxVolumeBackup{}),
+		KymaRef:        f.kymaRef,
+		KcpCluster:     f.kcpCluster,
+		SkrCluster:     f.skrCluster,
+		clientProvider: f.clientProvider,
 	}
 }
 
@@ -34,10 +56,14 @@ func newStateFactory(
 	baseStateFactory composed.StateFactory,
 	kymaRef klog.ObjectRef,
 	kcpCluster composed.StateCluster,
+	skrCluster composed.StateCluster,
+	clientProvider client2.ClientProvider[client.Client],
 ) *stateFactory {
 	return &stateFactory{
 		baseStateFactory: baseStateFactory,
 		kymaRef:          kymaRef,
 		kcpCluster:       kcpCluster,
+		skrCluster:       skrCluster,
+		clientProvider:   clientProvider,
 	}
 }
