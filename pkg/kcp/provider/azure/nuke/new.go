a5d708fd7bf81334e3873d4f8a11b38a634089bc
Ravi Shankar <ravi.shankar06@sap.com>
2025-03-21 18:23:37 -0700

    feat: NukeRwxBackup for Azure

---
+++ pkg/kcp/provider/azure/nuke/new.go
@@ -0,0 +1,46 @@
+package nuke
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/kyma-project/cloud-manager/api/cloud-control/v1beta1"
+	"github.com/kyma-project/cloud-manager/pkg/composed"
+	nuketypes "github.com/kyma-project/cloud-manager/pkg/kcp/nuke/types"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func New(stateFactory StateFactory) composed.Action {
+	return func(ctx context.Context, st composed.State) (error, context.Context) {
+		logger := composed.LoggerFromCtx(ctx)
+		state, err := stateFactory.NewState(ctx, st.(nuketypes.State))
+		if err != nil {
+			err = fmt.Errorf("error creating new Azure Nuke state: %w", err)
+			logger.Error(err, "Error")
+			obj := st.Obj().(*v1beta1.Nuke)
+			return composed.PatchStatus(obj).
+				SetExclusiveConditions(metav1.Condition{
+					Type:    v1beta1.ConditionTypeError,
+					Status:  metav1.ConditionTrue,
+					Reason:  v1beta1.ReasonCloudProviderError,
+					Message: err.Error(),
+				}).
+				SuccessError(composed.StopAndForget).
+				Run(ctx, st)
+		}
+		return composed.ComposeActions(
+			"azureNuke",
+			createAzureClient,
+			loadAzureBackups,
+			providerResourceStatusDiscovered,
+			deleteAzureBackups,
+			providerResourceStatusDeleting,
+			providerResourceStatusDeleted,
+			checkIfAllProviderResourcesDeleted,
+			// continue to parent action
+			func(ctx context.Context, state composed.State) (error, context.Context) {
+				return nil, ctx
+			},
+		)(ctx, state)
+	}
+}
---
+++ pkg/kcp/provider/azure/nuke/state.go
@@ -0,0 +1,73 @@
+package nuke
+
+import (
+	"context"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/recoveryservices/armrecoveryservices"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/recoveryservices/armrecoveryservicesbackup/v4"
+	"github.com/kyma-project/cloud-manager/api/cloud-control/v1beta1"
+	"github.com/kyma-project/cloud-manager/pkg/common/abstractions"
+	"github.com/kyma-project/cloud-manager/pkg/common/actions/focal"
+	nuketypes "github.com/kyma-project/cloud-manager/pkg/kcp/nuke/types"
+	azureClient "github.com/kyma-project/cloud-manager/pkg/kcp/provider/azure/client"
+	"github.com/kyma-project/cloud-manager/pkg/kcp/provider/azure/nuke/client"
+	"k8s.io/utils/ptr"
+)
+
+type StateFactory interface {
+	NewState(ctx context.Context, nukeState nuketypes.State) (focal.State, error)
+}
+
+func NewStateFactory(
+	azureClientProvider azureClient.ClientProvider[client.NukeRwxBackupClient],
+	env abstractions.Environment) StateFactory {
+	return stateFactory{
+		azureClientProvider: azureClientProvider,
+		env:                 env,
+	}
+}
+
+type stateFactory struct {
+	azureClientProvider azureClient.ClientProvider[client.NukeRwxBackupClient]
+	env                 abstractions.Environment
+}
+
+func (f stateFactory) NewState(ctx context.Context, nukeState nuketypes.State) (focal.State, error) {
+	return &State{
+		State:               nukeState,
+		azureClientProvider: f.azureClientProvider,
+		env:                 f.env,
+	}, nil
+}
+
+type State struct {
+	nuketypes.State
+	ProviderResources []*nuketypes.ProviderResourceKindState
+
+	azureClientProvider azureClient.ClientProvider[client.NukeRwxBackupClient]
+	env                 abstractions.Environment
+	azureClient         client.NukeRwxBackupClient
+
+	recoveryVaults []*armrecoveryservices.Vault
+	protectedItems []*armrecoveryservicesbackup.ProtectedItemResource
+}
+
+type azureProtectedItem struct {
+	*armrecoveryservicesbackup.ProtectedItemResource
+}
+
+func (b azureProtectedItem) GetId() string {
+	return ptr.Deref(b.ID, "")
+}
+
+func (b azureProtectedItem) GetObject() interface{} {
+	return b
+}
+
+type ProviderNukeStatus struct {
+	v1beta1.NukeStatus
+}
+
+func (s *State) GetSubscriptionId() string {
+	return s.Scope().Spec.Scope.Azure.SubscriptionId
+}
