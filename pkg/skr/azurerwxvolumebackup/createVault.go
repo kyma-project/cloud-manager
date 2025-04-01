bcaabbc58aedb381639131aa3a12af74caa572e7
Kevin Ho <kevin.ho01@sap.com>
2025-04-01 16:19:26 -0700

    moved vaultExist logic to the action

--- pkg/skr/azurerwxvolumebackup/createVault.go
+++ pkg/skr/azurerwxvolumebackup/createVault.go
@@ -3,8 +3,26 @@ package azurerwxvolumebackup
 import (
 	"context"
 	"fmt"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/recoveryservices/armrecoveryservices"
 	"github.com/kyma-project/cloud-manager/pkg/composed"
+	"slices"
 )
+
+func vaultExists(vaults []*armrecoveryservices.Vault, location string) bool {
+
+	return slices.ContainsFunc(vaults, func(vault *armrecoveryservices.Vault) bool {
+
+		if vault.Location == nil || vault.Tags == nil {
+			return false
+		}
+
+		_, tagExists := vault.Tags["cloud-manager"]
+
+		return *vault.Location == location && tagExists
+
+	})
+
+}
 
 func createVault(ctx context.Context, st composed.State) (error, context.Context) {
 
@@ -19,8 +37,18 @@ func createVault(ctx context.Context, st composed.State) (error, context.Context
 	location := backup.Spec.Location
 	vaultName := fmt.Sprintf("cm-vault-%s", location)
 
+	vaults, err := state.client.ListVaults(ctx)
+	if err != nil {
+		return composed.StopWithRequeue, ctx
+	}
+
+	// If exists, exit early and go next action
+	if vaultExists(vaults, location) {
+		return nil, ctx
+	}
+
 	// TODO: resp gives the jobId. Use to check status
-	_, err := state.client.CreateVault(ctx, resourceGroupName, vaultName, location)
+	_, err = state.client.CreateVault(ctx, resourceGroupName, vaultName, location)
 	if err != nil {
 		return composed.StopWithRequeue, ctx
 	}
