3e52c05172a5ab7cd2cfb4c803088e08b2f40861
Milos Tomic <milos.tomic@sap.com>
2025-04-10 10:49:02 +0200

    feat: expose cloud details part 1

--- pkg/common/objSupportsProvider_test.go
+++ pkg/common/objSupportsProvider_test.go
@@ -3,7 +3,7 @@ package common
 import (
 	"fmt"
 	"github.com/elliotchance/pie/v2"
-	cloudcontrol1beta1 "github.com/kyma-project/cloud-manager/api/cloud-control/v1beta1"
+	cloudcontrolv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-control/v1beta1"
 	cloudresourcesv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-resources/v1beta1"
 	"github.com/kyma-project/cloud-manager/pkg/common/objkind"
 	"github.com/stretchr/testify/assert"
@@ -21,17 +21,17 @@ import (
 func TestObjSupportsProvider(t *testing.T) {
 
 	const (
-		AWS       = cloudcontrol1beta1.ProviderAws
-		AZURE     = cloudcontrol1beta1.ProviderAzure
-		GCP       = cloudcontrol1beta1.ProviderGCP
-		OPENSTACK = cloudcontrol1beta1.ProviderOpenStack
+		AWS       = cloudcontrolv1beta1.ProviderAws
+		AZURE     = cloudcontrolv1beta1.ProviderAzure
+		GCP       = cloudcontrolv1beta1.ProviderGCP
+		OPENSTACK = cloudcontrolv1beta1.ProviderOpenStack
 	)
 	scheme := runtime.NewScheme()
 	assert.NoError(t, cloudresourcesv1beta1.AddToScheme(scheme))
 	assert.NoError(t, clientgoscheme.AddToScheme(scheme))
 
 	g := cloudresourcesv1beta1.GroupVersion.Group
-	//v := cloudresourcesv1beta1.GroupVersion.Version
+	v := cloudresourcesv1beta1.GroupVersion.Version
 
 	kCrd := "CustomResourceDefinition"
 
@@ -39,37 +39,34 @@ func TestObjSupportsProvider(t *testing.T) {
 	baseCrdUnstructured.SetAPIVersion(apiextensionsv1.SchemeGroupVersion.WithKind(kCrd).GroupVersion().String())
 	baseCrdUnstructured.SetKind(kCrd)
 
-	//gCm := ""
-	//kCm := "ConfigMap"
-
 	testData := []struct {
 		title              string
 		obj                client.Object
-		supportedProviders []cloudcontrol1beta1.ProviderType
+		supportedProviders []cloudcontrolv1beta1.ProviderType
 	}{
-		//{
-		//	"IpRange typed",
-		//	&cloudresourcesv1beta1.IpRange{},
-		//	[]cloudcontrol1beta1.ProviderType{AWS, AZURE, GCP},
-		//},
-		//{
-		//	"IpRange unstructured",
-		//	objkind.NewUnstructuredWithGVK(g, v, "IpRange"),
-		//	[]cloudcontrol1beta1.ProviderType{AWS, AZURE, GCP},
-		//},
+		{
+			"IpRange typed",
+			&cloudresourcesv1beta1.IpRange{},
+			[]cloudcontrolv1beta1.ProviderType{AWS, AZURE, GCP},
+		},
+		{
+			"IpRange unstructured",
+			objkind.NewUnstructuredWithGVK(g, v, "IpRange"),
+			[]cloudcontrolv1beta1.ProviderType{AWS, AZURE, GCP},
+		},
 		{
 			"IpRange CRD typed",
 			objkind.NewCrdTypedV1WithKindGroup(t, "IpRange", g),
-			[]cloudcontrol1beta1.ProviderType{AWS, AZURE, GCP},
+			[]cloudcontrolv1beta1.ProviderType{AWS, AZURE, GCP},
 		},
-		//{
-		//	"IpRange CRD unstructured",
-		//	objkind.NewCrdUnstructuredWithKindGroup(t, baseCrdUnstructured, "IpRange", g),
-		//	[]cloudcontrol1beta1.ProviderType{AWS, AZURE, GCP},
-		//},
+		{
+			"IpRange CRD unstructured",
+			objkind.NewCrdUnstructuredWithKindGroup(t, baseCrdUnstructured, "IpRange", g),
+			[]cloudcontrolv1beta1.ProviderType{AWS, AZURE, GCP},
+		},
 	}
 
-	allProviders := []cloudcontrol1beta1.ProviderType{AWS, AZURE, GCP, OPENSTACK}
+	allProviders := []cloudcontrolv1beta1.ProviderType{AWS, AZURE, GCP, OPENSTACK}
 
 	for _, data := range testData {
 		for _, provider := range allProviders {
--- pkg/skr/cceenfsvolume/waitKcpNfsInstanceStatus.go
+++ pkg/skr/cceenfsvolume/waitKcpNfsInstanceStatus.go
@@ -2,7 +2,7 @@ package cceenfsvolume
 
 import (
 	"context"
-	cloudcontrol1beta1 "github.com/kyma-project/cloud-manager/api/cloud-control/v1beta1"
+	cloudcontrolv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-control/v1beta1"
 	"github.com/kyma-project/cloud-manager/pkg/composed"
 	"github.com/kyma-project/cloud-manager/pkg/util"
 	"k8s.io/apimachinery/pkg/api/meta"
@@ -23,10 +23,10 @@ func waitKcpNfsInstanceStatus(ctx context.Context, st composed.State) (error, co
 		return nil, ctx
 	}
 
-	if meta.FindStatusCondition(*state.KcpNfsInstance.Conditions(), cloudcontrol1beta1.ConditionTypeReady) != nil {
+	if meta.FindStatusCondition(*state.KcpNfsInstance.Conditions(), cloudcontrolv1beta1.ConditionTypeReady) != nil {
 		return nil, ctx
 	}
-	if meta.FindStatusCondition(*state.KcpNfsInstance.Conditions(), cloudcontrol1beta1.ConditionTypeError) != nil {
+	if meta.FindStatusCondition(*state.KcpNfsInstance.Conditions(), cloudcontrolv1beta1.ConditionTypeError) != nil {
 		return nil, ctx
 	}
 
