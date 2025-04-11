c330e036a0cbbcba5e36053fb56f4442f7b52019
Ravi Shankar <ravi.shankar06@sap.com>
2025-04-11 16:41:33 -0700

    debug

--- pkg/kcp/provider/azure/nuke/disableSoftDelete.go
+++ pkg/kcp/provider/azure/nuke/disableSoftDelete.go
@@ -13,18 +13,24 @@ func disableSoftDelete(ctx context.Context, st composed.State) (error, context.C
 	state := st.(*State)
 	logger := composed.LoggerFromCtx(ctx)
 
-	logger.Info("disableSoftDelete")
 	for _, rks := range state.ProviderResources {
 		if rks.Kind == "AzureRecoveryVault" && rks.Provider == cloudcontrolv1beta1.ProviderAzure {
 			for _, obj := range rks.Objects {
 
 				item := obj.(azureVault)
-				softDelete := item.Properties.SecuritySettings.SoftDeleteSettings.SoftDeleteState
+				logger.Info(fmt.Sprintf("Vault Properties : %v", item.Properties))
+				var softDelete *armrecoveryservices.SoftDeleteState
+				if item.Properties.SecuritySettings != nil && item.Properties.SecuritySettings.SoftDeleteSettings != nil {
+					softDelete = item.Properties.SecuritySettings.SoftDeleteSettings.SoftDeleteState
 
-				if *softDelete != armrecoveryservices.SoftDeleteStateAlwaysON && *softDelete != armrecoveryservices.SoftDeleteStateEnabled {
+				}
+
+				if softDelete != nil && *softDelete != armrecoveryservices.SoftDeleteStateAlwaysON &&
+					*softDelete != armrecoveryservices.SoftDeleteStateEnabled {
 					continue
 				}
 
+				logger.Info(fmt.Sprintf("disableSoftDelete for vault :%s", *item.Name))
 				err := state.azureClient.DisableSoftDelete(ctx, item.Vault)
 				if err != nil {
 					logger.Error(err, fmt.Sprintf("Error Disabling SoftDelete on Azure Vault: %s", obj.GetId()))
