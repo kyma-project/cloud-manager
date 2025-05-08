222a75be00e1bce30a0be41f377d0db083d1390f
Dušan <dusan.pantic@sap.com>
2025-05-08 09:42:11 +0200

    test(cloud-resources): add preventDeleteOnGcpRedisClusterUsage GcpSubnet controller test

--- internal/controller/cloud-resources/gcpsubnet_test.go
+++ internal/controller/cloud-resources/gcpsubnet_test.go
@@ -1,10 +1,15 @@
 package cloudresources
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/kyma-project/cloud-manager/api"
+	"k8s.io/apimachinery/pkg/api/meta"
 
 	cloudcontrolv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-control/v1beta1"
 	cloudresourcesv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-resources/v1beta1"
+	"github.com/kyma-project/cloud-manager/pkg/skr/gcprediscluster"
 	. "github.com/kyma-project/cloud-manager/pkg/testinfra/dsl"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -63,7 +68,7 @@ var _ = Describe("Feature: SKR GcpSubnet", func() {
 			By("And has spec.scope.name equal to SKR Cluster kyma name")
 			Expect(kcpGcpSubnet.Spec.Scope.Name).To(Equal(infra.SkrKymaRef().Name))
 
-			By("And has spec.remoteRef matching to to SKR IpRange")
+			By("And has spec.remoteRef matching to to SKR GcpSubnet")
 			Expect(kcpGcpSubnet.Spec.RemoteRef.Name).To(Equal(gcpSubnet.Name))
 
 			By("And has spec.cidr equal to SKR GcpSubnet.spec.cidr")
@@ -214,6 +219,117 @@ var _ = Describe("Feature: SKR GcpSubnet", func() {
 				WithArguments(infra.Ctx(), infra.SKR().Client(), gcpSubnet).
 				Should(Succeed(), "expected GcpSubnet not to exist")
 		})
+	})
+
+	It("Scenario: SKR GcpSubnet can not be deleted if used by GcpRedisCluster", func() {
+		const (
+			skrGcpSubnetName       = "acc073fe-8a43-41cc-a566-08e99053a6fa"
+			skrGcpRedisClusterName = "2a34a935-7877-4456-9e73-963001c67463"
+		)
+		skrGcpSubnet := &cloudresourcesv1beta1.GcpSubnet{}
+		kcpGcpSubnet := &cloudcontrolv1beta1.GcpSubnet{}
+		skrGcpRedisCluster := &cloudresourcesv1beta1.GcpRedisCluster{}
+
+		By("Given SKR GcpSubnet exists", func() {
+			Eventually(CreateSkrGcpSubnet).
+				WithArguments(
+					infra.Ctx(), infra.SKR().Client(), skrGcpSubnet,
+					WithName(skrGcpSubnetName),
+					WithSkrGcpSubnetCidr(addressSpace.MustAllocate(22)),
+				).
+				Should(Succeed(), "failed creating SKR GcpSubnet")
+
+		})
+
+		By("And Given SKR GcpSubnet has Ready condition", func() {
+			// load SKR GcpSubnet to get ID
+			Eventually(LoadAndCheck).
+				WithArguments(
+					infra.Ctx(),
+					infra.SKR().Client(),
+					skrGcpSubnet,
+					NewObjActions(),
+					HavingGcpSubnetStatusId(),
+				).
+				Should(Succeed(), "expected SKR GcpSubnet to get status.id, but it didn't")
+
+			// KCP GcpSubnet is created by manager
+			Eventually(LoadAndCheck).
+				WithArguments(
+					infra.Ctx(),
+					infra.KCP().Client(),
+					kcpGcpSubnet,
+					NewObjActions(WithName(skrGcpSubnet.Status.Id)),
+				).
+				Should(Succeed(), "expected SKR GcpSubnet to be created, but none found")
+
+			// When Kcp GcpSubnet gets Ready condition
+			Eventually(UpdateStatus).
+				WithArguments(
+					infra.Ctx(),
+					infra.KCP().Client(),
+					kcpGcpSubnet,
+					WithConditions(KcpReadyCondition()),
+				).
+				Should(Succeed(), "failed updating KCP GcpSubnet status")
+
+			// And when SKR GcpSubnet gets Ready condition by manager
+			Eventually(LoadAndCheck).
+				WithArguments(
+					infra.Ctx(),
+					infra.SKR().Client(),
+					skrGcpSubnet,
+					NewObjActions(),
+					HavingConditionTrue(cloudresourcesv1beta1.ConditionTypeReady),
+				).
+				Should(Succeed(), "expected SKR GcpSubnet to get Ready condition, but it did not")
+		})
+
+		By("And Given GcpRedisCluster exists", func() {
+			// tell GcpRedisCluster reconciler to ignore this GcpRedisCluster
+			gcprediscluster.Ignore.AddName(skrGcpRedisClusterName)
+
+			Eventually(CreateSkrGcpRedisCluster).
+				WithArguments(infra.Ctx(), infra.SKR().Client(), skrGcpRedisCluster,
+					WithName(skrGcpRedisClusterName),
+					WithGcpSubnet(skrGcpSubnet.Name),
+					WithSkrGcpRedisClusterDefaultSpec(),
+				).
+				Should(Succeed(), "failed creating GcpRedisCluster")
+
+			time.Sleep(50 * time.Millisecond)
+		})
+
+		By("When SKR GcpSubnet is deleted", func() {
+			Eventually(Delete).
+				WithArguments(infra.Ctx(), infra.SKR().Client(), skrGcpSubnet).
+				Should(Succeed(), "failed deleting SKR GcpSubnet")
+		})
+
+		By("Then SKR GcpSubnet has Warning condition", func() {
+			Eventually(LoadAndCheck).
+				WithArguments(infra.Ctx(), infra.SKR().Client(), skrGcpSubnet, NewObjActions(), HavingConditionTrue(cloudresourcesv1beta1.ConditionTypeWarning)).
+				Should(Succeed(), "expected SKR GcpSubnet to have Warning condition")
+		})
+
+		By("And Then SKR GcpSubnet has DeleteWhileUsed reason", func() {
+			cond := meta.FindStatusCondition(skrGcpSubnet.Status.Conditions, cloudresourcesv1beta1.ConditionTypeWarning)
+			Expect(cond.Reason).To(Equal(cloudresourcesv1beta1.ConditionTypeDeleteWhileUsed))
+			Expect(cond.Message).To(Equal(fmt.Sprintf("Can not be deleted while used by: [%s/%s]", skrGcpRedisCluster.Namespace, skrGcpRedisCluster.Name)))
+		})
+
+		By(fmt.Sprintf("// cleanup: delete SKR GcpRedisCluster %s", skrGcpRedisCluster.Name), func() {
+			Eventually(Delete).
+				WithArguments(infra.Ctx(), infra.SKR().Client(), skrGcpRedisCluster).
+				Should(SucceedIgnoreNotFound(), "failed deleting SKR GcpRedisCluster to clean up")
+		})
+
+		By(fmt.Sprintf("// cleanup: delete SKR GcpSubnet %s", skrGcpSubnet.Name), func() {
+			Eventually(Delete).
+				WithArguments(infra.Ctx(), infra.SKR().Client(), skrGcpSubnet).
+				Should(SucceedIgnoreNotFound(), "failed deleting SKR GcpSubnet to clean up")
+		})
+
 	})
 
 })
