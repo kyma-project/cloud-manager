96c6fce087deb1f24d4fadd3f5a538a07c0bf88a
Dušan Pantić <dushanpantic@users.noreply.github.com>
2025-05-08 21:25:50 +0200

    refactor: change interface for Lease Acquire and Release (#1222)

---
+++ pkg/common/leases/lease.go
@@ -0,0 +1,102 @@
+package leases
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/kyma-project/cloud-manager/api"
+	"github.com/kyma-project/cloud-manager/pkg/composed"
+	coordinationv1 "k8s.io/api/coordination/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+type LeaseResult int
+
+const (
+	RenewedLease LeaseResult = iota
+	AcquiredLease
+	LeasingFailed
+	OtherLeased
+)
+
+func Acquire(ctx context.Context, cluster composed.StateCluster, leaseName, leaseNamespace, holderName string, leaseDurationSec int32) (LeaseResult, error) {
+	lease := &coordinationv1.Lease{}
+	err := cluster.K8sClient().Get(ctx, types.NamespacedName{Name: leaseName, Namespace: leaseNamespace}, lease)
+	if err != nil && !apierrors.IsNotFound(err) {
+		return LeasingFailed, err
+	}
+
+	if lease.Name == "" {
+		lease = &coordinationv1.Lease{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      leaseName,
+				Namespace: leaseNamespace,
+			},
+			Spec: coordinationv1.LeaseSpec{
+				HolderIdentity:       &holderName,
+				LeaseDurationSeconds: &leaseDurationSec,
+				AcquireTime:          &metav1.MicroTime{Time: time.Now()},
+				RenewTime:            &metav1.MicroTime{Time: time.Now()},
+			},
+		}
+		controllerutil.AddFinalizer(lease, api.CommonFinalizerDeletionHook)
+		err := cluster.K8sClient().Create(ctx, lease)
+		if err != nil {
+			return LeasingFailed, err
+		}
+		return AcquiredLease, nil
+	}
+	if ptr.Deref(lease.Spec.HolderIdentity, "") == holderName {
+		lease.Spec.RenewTime = &metav1.MicroTime{Time: time.Now()}
+		err := cluster.K8sClient().Update(ctx, lease)
+		if err != nil {
+			return LeasingFailed, err
+		}
+		return RenewedLease, nil
+	}
+	leaseDurationSeconds := ptr.Deref(lease.Spec.LeaseDurationSeconds, leaseDurationSec)
+	if lease.Spec.RenewTime == nil || lease.Spec.RenewTime.Time.Add(time.Second*time.Duration(leaseDurationSeconds)).Before(time.Now()) {
+		//lease expired
+		lease.Spec.RenewTime = &metav1.MicroTime{Time: time.Now()}
+		lease.Spec.HolderIdentity = &holderName
+		err := cluster.K8sClient().Update(ctx, lease)
+		if err != nil {
+			return LeasingFailed, err
+		}
+		return AcquiredLease, nil
+	}
+	return OtherLeased, nil
+}
+
+func Release(ctx context.Context, cluster composed.StateCluster, leaseName, leaseNamespace, holderName string) error {
+	lease := &coordinationv1.Lease{}
+	err := cluster.K8sClient().Get(ctx, types.NamespacedName{Name: leaseName, Namespace: leaseNamespace}, lease)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return nil
+		}
+		return err
+	}
+
+	ownerName := ptr.Deref(lease.Spec.HolderIdentity, "")
+
+	if ownerName != holderName {
+		return fmt.Errorf("lease %s/%s belongs to another owner (%s)", leaseName, leaseNamespace, holderName)
+	}
+
+	controllerutil.RemoveFinalizer(lease, api.CommonFinalizerDeletionHook)
+	err = cluster.K8sClient().Update(ctx, lease)
+	if err != nil {
+		return err
+	}
+	err = cluster.K8sClient().Delete(ctx, lease)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- pkg/skr/gcpnfsvolumerestore/releaseLease.go
+++ pkg/skr/gcpnfsvolumerestore/releaseLease.go
@@ -2,8 +2,10 @@ package gcpnfsvolumerestore
 
 import (
 	"context"
+	"strings"
+
+	"github.com/kyma-project/cloud-manager/pkg/common/leases"
 	"github.com/kyma-project/cloud-manager/pkg/composed"
-	"github.com/kyma-project/cloud-manager/pkg/skr/common/leases"
 	"github.com/kyma-project/cloud-manager/pkg/util"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
@@ -13,11 +15,30 @@ func releaseLease(ctx context.Context, st composed.State) (error, context.Contex
 	//If it reaches here, the operation is in final status
 	state := st.(*State)
 	restore := state.ObjAsGcpNfsVolumeRestore()
-	err := leases.Release(ctx, state.SkrCluster,
-		restore.Spec.Destination.Volume.ToNamespacedName(restore.Namespace),
-		types.NamespacedName{Name: restore.Name, Namespace: restore.Namespace},
-		"restore")
-	if err != nil && !apierrors.IsNotFound(err) {
+	volumeNamespacedName := restore.Spec.Destination.Volume.ToNamespacedName(restore.Namespace)
+
+	leaseName := getLeaseName(volumeNamespacedName.Name, "restore")
+	leaseNamespace := volumeNamespacedName.Namespace
+	holderName := getHolderName(types.NamespacedName{Name: restore.Name, Namespace: restore.Namespace})
+
+	err := leases.Release(
+		ctx,
+		state.SkrCluster,
+		leaseName,
+		leaseNamespace,
+		holderName,
+	)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return nil, nil
+		}
+
+		if strings.Contains(err.Error(), "belongs to another owner") {
+			logger := composed.LoggerFromCtx(ctx)
+			logger.Info("Ignoring expected ownership error.")
+			return nil, nil
+		}
+
 		return composed.LogErrorAndReturn(err, "Error releasing lease", composed.StopWithRequeueDelay(util.Timing.T100ms()), ctx)
 	}
 	return nil, nil
