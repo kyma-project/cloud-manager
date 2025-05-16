f221afdac4cbaa8efde3d9a7a99137505078f2c4
Vladimir Andjelkoski <vladimir.andjelkoski@sap.com>
2025-05-16 11:42:25 +0200

    feat(AzureVNetLink): skr reconciler

---
+++ pkg/testinfra/dsl/azureVNetLink.go
@@ -0,0 +1,51 @@
+package dsl
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	cloudresourcesv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-resources/v1beta1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func CreateAzureVNetLink(ctx context.Context, clnt client.Client, obj *cloudresourcesv1beta1.AzureVNetLink, opts ...ObjAction) error {
+	NewObjActions(opts...).
+		Append(
+			WithNamespace(DefaultSkrNamespace),
+		).
+		ApplyOnObject(obj)
+
+	err := clnt.Create(ctx, obj)
+	return err
+}
+
+func WithAzureRemoteVNetLinkName(remoteVNetLinkName string) ObjAction {
+	return &objAction{
+		f: func(obj client.Object) {
+			x := obj.(*cloudresourcesv1beta1.AzureVNetLink)
+			x.Spec.RemoteVirtualPrivateLinkName = remoteVNetLinkName
+		},
+	}
+}
+
+func WithAzureRemotePrivateDnsZone(remotePrivateDnsZone string) ObjAction {
+	return &objAction{
+		f: func(obj client.Object) {
+			x := obj.(*cloudresourcesv1beta1.AzureVNetLink)
+			x.Spec.RemotePrivateDnsZone = remotePrivateDnsZone
+		},
+	}
+}
+
+func AssertAzureVNetPeeringHasId() ObjAssertion {
+	return func(obj client.Object) error {
+		x, ok := obj.(*cloudresourcesv1beta1.AzureVNetLink)
+		if !ok {
+			return fmt.Errorf("the object %T is not  AzureVNetLink", obj)
+		}
+		if x.Status.Id == "" {
+			return errors.New("the AzureVNetLink ID not set")
+		}
+		return nil
+	}
+}
