cc3ad3caf5620b89cd29ddfc0cbe4eedfbb2da2c
Vladimir Andjelkoski <vladimir.andjelkoski@sap.com>
2025-07-07 17:16:49 +0200

    chore: change feature name

--- pkg/feature/types/types.go
+++ pkg/feature/types/types.go
@@ -28,12 +28,12 @@ type FeatureName = string
 const (
 	FeatureUnknown FeatureName = "unknown"
 
-	FeatureNfs             FeatureName = "nfs"
-	FeatureNfsBackup       FeatureName = "nfsBackup"
-	FeaturePeering         FeatureName = "peering"
-	FeatureRedis           FeatureName = "redis"
-	FeatureRedisCluster    FeatureName = "rediscluster"
-	FeatureAzureVpcDnsLink FeatureName = "azurevnetlink"
+	FeatureNfs          FeatureName = "nfs"
+	FeatureNfsBackup    FeatureName = "nfsBackup"
+	FeaturePeering      FeatureName = "peering"
+	FeatureRedis        FeatureName = "redis"
+	FeatureRedisCluster FeatureName = "rediscluster"
+	FeatureVpcDnsLink   FeatureName = "vpcdnslink"
 )
 
 type PlaneName = string
