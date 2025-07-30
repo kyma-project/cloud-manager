267147d887b06ccd988eac6aa280be0472c572ce
Kevin Ho <kevin.ho01@sap.com>
2025-07-30 16:09:42 -0700

    added sec-scanner-config version bump

---
+++ .github/workflows/scripts/render-sec-scanners-config.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Error handling.
+set -o nounset  # treat unset variables as an error and exit immediately.
+set -o errexit  # exit immediately when a command fails.
+set -E          # needs to be set if we want the ERR trap
+set -o pipefail # prevents errors in a pipeline from being masked
+
+# This scrpit generates the sec-scanners-config by fetching all relevant images.
+
+TAG=$1
+OUTPUT_FILE="sec-scanners-config.yaml"
+
+# Generating File.
+echo -e "generating to ${OUTPUT_FILE} \n"
+cat <<EOF | tee "${OUTPUT_FILE}"
+module-name: cloud-manager
+kind: kyma
+rc-tag: ${TAG}
+bdba:
+  - europe-docker.pkg.dev/kyma-project/prod/cloud-manager:main
+  - europe-docker.pkg.dev/kyma-project/prod/cloud-manager:${TAG}
+mend:
+  language: golang-mod
+  exclude:
+    - "**/*_test.go"
+checkmarx-one:
+  preset: go-default
+  exclude:
+    - '**/*_test.go'
+    - 'pkg/testinfra/**'
+EOF
