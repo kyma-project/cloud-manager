d433c9f7f8b7bf011484a6fdb3ecffc3cfbb6ec3
Dušan <dusan.pantic@sap.com>
2025-09-04 11:51:54 +0200

    chore: add Apply method to clientSpy

--- pkg/testinfra/clientspy/clientSpyFactory.go
+++ pkg/testinfra/clientspy/clientSpyFactory.go
@@ -27,6 +27,10 @@ type clientSpy struct {
 	patchCallCount  int64
 }
 
+func (clientSpy *clientSpy) Apply(ctx context.Context, obj runtime.ApplyConfiguration, opts ...client.ApplyOption) error {
+	return clientSpy.client.Apply(ctx, obj, opts...)
+}
+
 func (s *clientSpy) Create(ctx context.Context, obj client.Object, opts ...client.CreateOption) error {
 	s.createCallCount++
 	return s.client.Create(ctx, obj, opts...)
