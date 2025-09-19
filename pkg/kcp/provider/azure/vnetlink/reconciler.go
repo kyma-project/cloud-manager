ab07b8786c14d36a660c8aee6e614e544d237858
Vladimir Andjelkoski <vladimir.andjelkoski@sap.com>
2025-09-19 11:19:30 +0200

    feat(VpcDnsLink): DNS zone predicate

--- pkg/kcp/provider/azure/vnetlink/reconciler.go
+++ pkg/kcp/provider/azure/vnetlink/reconciler.go
@@ -2,9 +2,11 @@ package vnetlink
 
 import (
 	"context"
+	"github.com/kyma-project/cloud-manager/api/cloud-control/v1beta1"
 	cloudcontrolv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-control/v1beta1"
 	"github.com/kyma-project/cloud-manager/pkg/common/actions/focal"
 	"github.com/kyma-project/cloud-manager/pkg/composed"
+	"github.com/kyma-project/cloud-manager/pkg/feature"
 	"github.com/kyma-project/cloud-manager/pkg/kcp/provider/azure/vnetlink/dnszone"
 	"github.com/kyma-project/cloud-manager/pkg/util"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -50,8 +52,18 @@ func (r *azureVNetLinkReconciler) Reconcile(ctx context.Context, request ctrl.Re
 func (r *azureVNetLinkReconciler) newAction() composed.Action {
 	return composed.ComposeActions(
 		"main",
+		feature.LoadFeatureContextFromObj(&cloudcontrolv1beta1.AzureVNetLink{}),
 		focal.New(),
-		dnszone.New(r.dnsZoneStateFactory),
+		func(ctx context.Context, st composed.State) (error, context.Context) {
+			return composed.ComposeActions(
+				"vnetlinkCommon",
+				composed.BuildSwitchAction(
+					"zoneResolverSwitch",
+					nil,
+					composed.NewCase(dnsZonePredicate, dnszone.New(r.dnsZoneStateFactory)),
+				),
+			)(ctx, newState(st.(focal.State)))
+		},
 	)
 }
 
@@ -59,4 +71,19 @@ func (r *azureVNetLinkReconciler) newFocalState(name types.NamespacedName) focal
 	return r.focalStateFactory.NewState(
 		r.composedStateFactory.NewState(name, &cloudcontrolv1beta1.AzureVNetLink{}),
 	)
+}
+
+func dnsZonePredicate(_ context.Context, st composed.State) bool {
+	if link, ok := st.Obj().(*v1beta1.AzureVNetLink); ok {
+		return len(link.Spec.RemotePrivateDnsZone) > 0
+	}
+	return false
+}
+
+func dnsResolverPredicate(_ context.Context, st composed.State) bool {
+	if link, ok := st.Obj().(*v1beta1.AzureVNetLink); ok {
+		return len(link.Spec.RemoteDnsForwardingRuleset) > 0
+	}
+	return false
+
 }
