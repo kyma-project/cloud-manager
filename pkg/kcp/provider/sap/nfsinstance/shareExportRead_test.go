6618123a1c3f8a70d35c960d99fd32daca598008
Milos Tomic <milos.tomic@sap.com>
2025-09-19 11:15:20 +0200

    fix: ccee list access & export locations

--- pkg/kcp/provider/sap/nfsinstance/shareExportRead_test.go
+++ pkg/kcp/provider/sap/nfsinstance/shareExportRead_test.go
@@ -8,22 +8,22 @@ import (
 
 func TestParseExportUrl(t *testing.T) {
 	testData := []struct {
-		url string
+		url  string
 		host string
 		path string
-	} {
+	}{
 		{
-			url: "10.11.12.13:/share_1c3da1a4_3263_4779_b230_f9d496326dc6",
+			url:  "10.11.12.13:/share_1c3da1a4_3263_4779_b230_f9d496326dc6",
 			host: "10.11.12.13",
 			path: "share_1c3da1a4_3263_4779_b230_f9d496326dc6",
 		},
 		{
-			url: "10.11.12.13/share_1c3da1a4_3263_4779_b230_f9d496326dc6",
+			url:  "10.11.12.13/share_1c3da1a4_3263_4779_b230_f9d496326dc6",
 			host: "10.11.12.13",
 			path: "share_1c3da1a4_3263_4779_b230_f9d496326dc6",
 		},
 		{
-			url: "example.com/aaa/bbb",
+			url:  "example.com/aaa/bbb",
 			host: "example.com",
 			path: "aaa/bbb",
 		},
