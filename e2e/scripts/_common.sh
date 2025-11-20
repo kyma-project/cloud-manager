b730963fc909bf9a1c2db75f64918f050ff375c4
Milos Tomic <milos.tomic@sap.com>
2025-11-20 10:36:35 +0100

    chore: e2e infra gcp

---
+++ e2e/scripts/_common.sh
@@ -0,0 +1,89 @@
+
+log() {
+  DT=$(date "+%Y-%m-%dT%H:%M:%S")
+  echo "$DT $1"
+}
+
+checkRequiredCommands() {
+  local REQUIRED_COMMANDS=$1
+  for cmd in $REQUIRED_COMMANDS; do
+    if ! command -v $cmd > /dev/null ; then
+      echo "Command $cmd not found"
+      exit 1
+    fi
+  done
+}
+
+initRoleFileDefault() {
+  local DEFAULT_VALUE=$1
+  ROLE_FILE_DEFAULT="${ROLE_FILE_DEFAULT:-$DEFAULT_VALUE}"
+  case $ROLE_FILE_DEFAULT in (/*) : ;; (*) ROLE_FILE_DEFAULT="${SCRIPT_DIR}/${ROLE_FILE_DEFAULT}" ;; esac
+  if [ ! -f "$ROLE_FILE_DEFAULT" ]; then
+    echo "ROLE_FILE_DEFAULT $ROLE_FILE_DEFAULT not found"
+    exit 1
+  fi
+}
+
+initRoleFilePeering() {
+  local DEFAULT_VALUE=$1
+  ROLE_FILE_PEERING="${ROLE_FILE_PEERING:-$DEFAULT_VALUE}"
+  case $ROLE_FILE_PEERING in (/*) : ;; (*) ROLE_FILE_PEERING="${SCRIPT_DIR}/${ROLE_FILE_PEERING}" ;; esac
+  if [ ! -f "$ROLE_FILE_PEERING" ]; then
+    echo "ROLE_FILE_PEERING $ROLE_FILE_PEERING not found"
+    exit 1
+  fi
+}
+
+initSANameDefault() {
+  local DEFAULT_VALUE=$1
+  SA_NAME_DEFAULT="${SA_NAME_DEFAULT:-$DEFAULT_VALUE}"
+}
+
+initSANamePeering() {
+  local DEFAULT_VALUE=$1
+  SA_NAME_PEERING="${SA_NAME_PEERING:-$DEFAULT_VALUE}"
+}
+
+initRoleNameDefault() {
+  local DEFAULT_VALUE=$1
+  ROLE_NAME_DEFAULT="${ROLE_NAME_DEFAULT:-$DEFAULT_VALUE}"
+}
+
+initRoleNamePeering() {
+  local DEFAULT_VALUE=$1
+  ROLE_NAME_PEERING="${ROLE_NAME_PEERING:-$DEFAULT_VALUE}"
+}
+
+# GCP Specific
+
+gcpValidateProject() {
+  if [ -z "${GCP_PROJECT+x}" ]; then
+    echo "GCP_PROJECT is not set"
+    exit 1
+  fi
+}
+
+gcpInit() {
+  checkRequiredCommands 'gcloud jq'
+
+  gcpValidateProject
+
+  initRoleFileDefault '../../docs/contributor/permissions/gcp_default.yaml'
+  initRoleFilePeering '../../docs/contributor/permissions/gcp_peering.yaml'
+
+  initSANameDefault 'cloud-manager-e2e'
+  initSANamePeering 'cloud-manager-peering-e2e'
+
+  initRoleNameDefault 'cloud_manager_e2e'
+  initRoleNamePeering 'cloud_manager_peering_e2e'
+
+  echo "GCP_PROJECT=$GCP_PROJECT"
+  echo "SA_NAME_DEFAULT=$SA_NAME_DEFAULT"
+  echo "ROLE_NAME_DEFAULT=$ROLE_NAME_DEFAULT"
+  echo "ROLE_FILE_DEFAULT=$ROLE_FILE_DEFAULT"
+  echo "SA_NAME_PEERING=$SA_NAME_PEERING"
+  echo "ROLE_NAME_PEERING=$ROLE_NAME_PEERING"
+  echo "ROLE_FILE_PEERING=$ROLE_FILE_PEERING"
+  echo ""
+
+}
