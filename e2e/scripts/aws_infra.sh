e764d395761dd29a2ca90d53ad10cd318b0503fb
Milos Tomic <59831542+tmilos77@users.noreply.github.com>
2025-11-21 15:11:31 +0100

    chore: e2e infra gcp refactoring (#1537)
    Co-authored-by: Grzegorz Karaluch <grzegorz.karaluch@sap.com>

--- e2e/scripts/aws_infra.sh
+++ e2e/scripts/aws_infra.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
 
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source $SCRIPT_DIR/.env.aws
+source $SCRIPT_DIR/_common.sh
+
+AWS_ACCOUNT=$(aws sts get-caller-identity --query Account --output text)
+
+log "Running on AWS account $AWS_ACCOUNT"
