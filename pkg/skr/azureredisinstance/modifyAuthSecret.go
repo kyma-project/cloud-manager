b74342a2ad7e75d98bd9a004eedc76b1c80f2498
Stefan Kikic <stefan.kikic@sap.com>
2025-12-05 13:36:06 +0100

    feat: Add modifyAuthSecret support for Azure Redis resources

---
+++ pkg/skr/azureredisinstance/modifyAuthSecret.go
@@ -0,0 +1,37 @@
+package azureredisinstance
+
+import (
+	"bytes"
+	"context"
+	"maps"
+
+	"github.com/kyma-project/cloud-manager/pkg/composed"
+)
+
+func modifyAuthSecret(ctx context.Context, st composed.State) (error, context.Context) {
+	state := st.(*State)
+	logger := composed.LoggerFromCtx(ctx)
+
+	if state.AuthSecret == nil {
+		logger.Info("cant modify auth secret, not found")
+		return nil, ctx
+	}
+
+	currentSecretData := state.AuthSecret.Data
+	desiredSecretData := state.GetAuthSecretData()
+
+	if maps.EqualFunc(currentSecretData, desiredSecretData, func(l, r []byte) bool { return bytes.Equal(l, r) }) {
+		return nil, ctx
+	}
+
+	state.AuthSecret.Data = desiredSecretData
+
+	err := state.Cluster().K8sClient().Update(ctx, state.AuthSecret)
+	if err != nil {
+		return composed.LogErrorAndReturn(err, "Error updating secret for AzureRedisInstance", composed.StopWithRequeue, ctx)
+	}
+
+	logger.Info("AuthSecret for AzureRedisInstance updated")
+
+	return nil, ctx
+}
