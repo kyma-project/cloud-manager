4d0bc6133ac4f73b804dae1a6cf7561d2a6cc130
Stefan Kikic <stefan.kikic@sap.com>
2025-12-08 17:54:32 +0100

    feat: Add module label to Azure Redis KCP resources

--- pkg/skr/azureredisinstance/createKcpRedisInstance.go
+++ pkg/skr/azureredisinstance/createKcpRedisInstance.go
@@ -5,6 +5,7 @@ import (
 
 	cloudcontrolv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-control/v1beta1"
 	cloudresourcesv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-resources/v1beta1"
+	"github.com/kyma-project/cloud-manager/pkg/common"
 	"github.com/kyma-project/cloud-manager/pkg/composed"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -43,6 +44,9 @@ func createKcpRedisInstance(ctx context.Context, st composed.State) (error, cont
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      azureRedisInstance.Status.Id,
 			Namespace: state.KymaRef.Namespace,
+			Labels: map[string]string{
+				common.LabelKymaModule: "cloud-manager",
+			},
 			Annotations: map[string]string{
 				cloudcontrolv1beta1.LabelKymaName:        state.KymaRef.Name,
 				cloudcontrolv1beta1.LabelRemoteName:      azureRedisInstance.Name,
