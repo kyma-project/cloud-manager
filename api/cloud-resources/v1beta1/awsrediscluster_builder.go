03ddbfd8d4a570db5756e70bba0d1f68382ae548
Stefan Kikic <stefan.kikic@sap.com>
2025-12-11 10:01:38 +0100

    refactor: move Redis resource builders to API package

---
+++ api/cloud-resources/v1beta1/awsrediscluster_builder.go
@@ -0,0 +1,96 @@
+package v1beta1
+
+func NewAwsRedisClusterBuilder() *AwsRedisClusterBuilder {
+	return &AwsRedisClusterBuilder{
+		AwsRedisCluster: AwsRedisCluster{
+			Spec: AwsRedisClusterSpec{
+				RedisTier:        AwsRedisTierC1,
+				EngineVersion:    "7.0",
+				AuthEnabled:      true,
+				ShardCount:       2,
+				ReplicasPerShard: 1,
+			},
+		},
+	}
+}
+
+type AwsRedisClusterBuilder struct {
+	AwsRedisCluster AwsRedisCluster
+}
+
+func (b *AwsRedisClusterBuilder) Reset() *AwsRedisClusterBuilder {
+	b.AwsRedisCluster = AwsRedisCluster{}
+	return b
+}
+
+func (b *AwsRedisClusterBuilder) WithIpRange(ipRangeName string) *AwsRedisClusterBuilder {
+	b.AwsRedisCluster.Spec.IpRange.Name = ipRangeName
+	return b
+}
+
+func (b *AwsRedisClusterBuilder) WithRedisTier(redisTier AwsRedisClusterTier) *AwsRedisClusterBuilder {
+	b.AwsRedisCluster.Spec.RedisTier = redisTier
+	return b
+}
+
+func (b *AwsRedisClusterBuilder) WithEngineVersion(engineVersion string) *AwsRedisClusterBuilder {
+	b.AwsRedisCluster.Spec.EngineVersion = engineVersion
+	return b
+}
+
+func (b *AwsRedisClusterBuilder) WithAuthEnabled(authEnabled bool) *AwsRedisClusterBuilder {
+	b.AwsRedisCluster.Spec.AuthEnabled = authEnabled
+	return b
+}
+
+func (b *AwsRedisClusterBuilder) WithShardCount(shardCount int32) *AwsRedisClusterBuilder {
+	b.AwsRedisCluster.Spec.ShardCount = shardCount
+	return b
+}
+
+func (b *AwsRedisClusterBuilder) WithReplicasPerShard(replicasPerShard int32) *AwsRedisClusterBuilder {
+	b.AwsRedisCluster.Spec.ReplicasPerShard = replicasPerShard
+	return b
+}
+
+func (b *AwsRedisClusterBuilder) WithAuthSecret(name string, labels, annotations, extraData map[string]string) *AwsRedisClusterBuilder {
+	b.AwsRedisCluster.Spec.AuthSecret = &RedisAuthSecretSpec{
+		Name:        name,
+		Labels:      labels,
+		Annotations: annotations,
+		ExtraData:   extraData,
+	}
+	return b
+}
+
+func (b *AwsRedisClusterBuilder) WithAuthSecretName(name string) *AwsRedisClusterBuilder {
+	if b.AwsRedisCluster.Spec.AuthSecret == nil {
+		b.AwsRedisCluster.Spec.AuthSecret = &RedisAuthSecretSpec{}
+	}
+	b.AwsRedisCluster.Spec.AuthSecret.Name = name
+	return b
+}
+
+func (b *AwsRedisClusterBuilder) WithAuthSecretLabels(labels map[string]string) *AwsRedisClusterBuilder {
+	if b.AwsRedisCluster.Spec.AuthSecret == nil {
+		b.AwsRedisCluster.Spec.AuthSecret = &RedisAuthSecretSpec{}
+	}
+	b.AwsRedisCluster.Spec.AuthSecret.Labels = labels
+	return b
+}
+
+func (b *AwsRedisClusterBuilder) WithAuthSecretAnnotations(annotations map[string]string) *AwsRedisClusterBuilder {
+	if b.AwsRedisCluster.Spec.AuthSecret == nil {
+		b.AwsRedisCluster.Spec.AuthSecret = &RedisAuthSecretSpec{}
+	}
+	b.AwsRedisCluster.Spec.AuthSecret.Annotations = annotations
+	return b
+}
+
+func (b *AwsRedisClusterBuilder) WithAuthSecretExtraData(extraData map[string]string) *AwsRedisClusterBuilder {
+	if b.AwsRedisCluster.Spec.AuthSecret == nil {
+		b.AwsRedisCluster.Spec.AuthSecret = &RedisAuthSecretSpec{}
+	}
+	b.AwsRedisCluster.Spec.AuthSecret.ExtraData = extraData
+	return b
+}
---
+++ api/cloud-resources/v1beta1/gcprediscluster_builder.go
@@ -0,0 +1,92 @@
+package v1beta1
+
+func NewGcpRedisClusterBuilder() *GcpRedisClusterBuilder {
+	return &GcpRedisClusterBuilder{
+		GcpRedisCluster: GcpRedisCluster{
+			Spec: GcpRedisClusterSpec{
+				RedisTier: GcpRedisClusterTierC1,
+				RedisConfigs: map[string]string{
+					"maxmemory-policy": "allkeys-lru",
+				},
+				ShardCount:       2,
+				ReplicasPerShard: 1,
+			},
+		},
+	}
+}
+
+type GcpRedisClusterBuilder struct {
+	GcpRedisCluster GcpRedisCluster
+}
+
+func (b *GcpRedisClusterBuilder) Reset() *GcpRedisClusterBuilder {
+	b.GcpRedisCluster = GcpRedisCluster{}
+	return b
+}
+
+func (b *GcpRedisClusterBuilder) WithSubnet(subnetName string) *GcpRedisClusterBuilder {
+	b.GcpRedisCluster.Spec.Subnet.Name = subnetName
+	return b
+}
+
+func (b *GcpRedisClusterBuilder) WithRedisTier(redisTier GcpRedisClusterTier) *GcpRedisClusterBuilder {
+	b.GcpRedisCluster.Spec.RedisTier = redisTier
+	return b
+}
+
+func (b *GcpRedisClusterBuilder) WithRedisConfigs(redisConfigs map[string]string) *GcpRedisClusterBuilder {
+	b.GcpRedisCluster.Spec.RedisConfigs = redisConfigs
+	return b
+}
+
+func (b *GcpRedisClusterBuilder) WithShardCount(shardCount int32) *GcpRedisClusterBuilder {
+	b.GcpRedisCluster.Spec.ShardCount = shardCount
+	return b
+}
+
+func (b *GcpRedisClusterBuilder) WithReplicasPerShard(replicasPerShard int32) *GcpRedisClusterBuilder {
+	b.GcpRedisCluster.Spec.ReplicasPerShard = replicasPerShard
+	return b
+}
+
+func (b *GcpRedisClusterBuilder) WithAuthSecret(name string, labels, annotations, extraData map[string]string) *GcpRedisClusterBuilder {
+	b.GcpRedisCluster.Spec.AuthSecret = &RedisAuthSecretSpec{
+		Name:        name,
+		Labels:      labels,
+		Annotations: annotations,
+		ExtraData:   extraData,
+	}
+	return b
+}
+
+func (b *GcpRedisClusterBuilder) WithAuthSecretName(name string) *GcpRedisClusterBuilder {
+	if b.GcpRedisCluster.Spec.AuthSecret == nil {
+		b.GcpRedisCluster.Spec.AuthSecret = &RedisAuthSecretSpec{}
+	}
+	b.GcpRedisCluster.Spec.AuthSecret.Name = name
+	return b
+}
+
+func (b *GcpRedisClusterBuilder) WithAuthSecretLabels(labels map[string]string) *GcpRedisClusterBuilder {
+	if b.GcpRedisCluster.Spec.AuthSecret == nil {
+		b.GcpRedisCluster.Spec.AuthSecret = &RedisAuthSecretSpec{}
+	}
+	b.GcpRedisCluster.Spec.AuthSecret.Labels = labels
+	return b
+}
+
+func (b *GcpRedisClusterBuilder) WithAuthSecretAnnotations(annotations map[string]string) *GcpRedisClusterBuilder {
+	if b.GcpRedisCluster.Spec.AuthSecret == nil {
+		b.GcpRedisCluster.Spec.AuthSecret = &RedisAuthSecretSpec{}
+	}
+	b.GcpRedisCluster.Spec.AuthSecret.Annotations = annotations
+	return b
+}
+
+func (b *GcpRedisClusterBuilder) WithAuthSecretExtraData(extraData map[string]string) *GcpRedisClusterBuilder {
+	if b.GcpRedisCluster.Spec.AuthSecret == nil {
+		b.GcpRedisCluster.Spec.AuthSecret = &RedisAuthSecretSpec{}
+	}
+	b.GcpRedisCluster.Spec.AuthSecret.ExtraData = extraData
+	return b
+}
