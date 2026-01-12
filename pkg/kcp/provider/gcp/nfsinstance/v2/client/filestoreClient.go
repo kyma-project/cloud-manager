f7fbc73ba76f551940488210531252a0c0cfffd3
Dušan <dusan.pantic@sap.com>
2026-01-12 13:24:19 +0100

    fix update

--- pkg/kcp/provider/gcp/nfsinstance/v2/client/filestoreClient.go
+++ pkg/kcp/provider/gcp/nfsinstance/v2/client/filestoreClient.go
@@ -105,6 +105,9 @@ func (c *filestoreClient) CreateInstance(ctx context.Context, projectId, locatio
 func (c *filestoreClient) UpdateInstance(ctx context.Context, projectId, location, instanceId string, instance *filestorepb.Instance, updateMask []string) (string, error) {
 	logger := composed.LoggerFromCtx(ctx)
 
+	// Set the Name field - required for UpdateInstance API
+	instance.Name = formatInstanceName(projectId, location, instanceId)
+
 	req := &filestorepb.UpdateInstanceRequest{
 		UpdateMask: &fieldmaskpb.FieldMask{
 			Paths: updateMask,
