e673dbed3f080288928627f1a14d0cc4ca5fe3d4
Vladimir Andjelkoski <vladimir.andjelkoski@sap.com>
2026-01-15 15:34:56 +0100

    chore: http operation illegal option -L

--- e2e/http.go
+++ e2e/http.go
@@ -33,8 +33,9 @@ func (h *HttpOperation) Validate() error {
 
 func (h *HttpOperation) Args() []string {
 	result := []string{
-		"-L", // follow location 3xx redicts
+		"-L", // follow location 3xx redirects
 		"-m",
+		fmt.Sprintf("%d", h.MaxTime),
 	}
 	if h.Method != "" {
 		result = append(result, "-X", h.Method)
