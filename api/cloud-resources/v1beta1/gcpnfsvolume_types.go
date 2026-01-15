23887a5a7ff9d35185933b7a63eed71d83bbca98
Dušan <dusan.pantic@sap.com>
2026-01-15 20:42:16 +0100

    simplify CEL rules

--- api/cloud-resources/v1beta1/gcpnfsvolume_types.go
+++ api/cloud-resources/v1beta1/gcpnfsvolume_types.go
@@ -63,16 +63,16 @@ const (
 )
 
 // GcpNfsVolumeSpec defines the desired state of GcpNfsVolume
-// +kubebuilder:validation:XValidation:rule=(self.tier != "REGIONAL" || self.tier == "REGIONAL" && (self.capacityGb >= 1024 && self.capacityGb <= 9984 && (self.capacityGb - 1024) % 256 == 0 || self.capacityGb >= 10240 && self.capacityGb <= 102400 && (self.capacityGb - 10240) % 2560 == 0)), message="REGIONAL tier capacityGb must be between 1024 and 9984, and it must be divisble by 256, or between 10240 and 102400, and divisible by 2560"
-// +kubebuilder:validation:XValidation:rule=(self.tier != "REGIONAL" || self.tier == "REGIONAL" && size(self.fileShareName) <= 32), message="REGIONAL tier fileShareName length must be 32 or less characters"
-// +kubebuilder:validation:XValidation:rule=(self.tier != "ZONAL" || self.tier == "ZONAL" && (self.capacityGb >= 1024 && self.capacityGb <= 9984 && (self.capacityGb - 1024) % 256 == 0 || self.capacityGb >= 10240 && self.capacityGb <= 102400 && (self.capacityGb - 10240) % 2560 == 0)), message="ZONAL tier capacityGb must be between 1024 and 9984, and it must be divisble by 256, or between 10240 and 102400, and divisible by 2560"
-// +kubebuilder:validation:XValidation:rule=(self.tier != "ZONAL" || self.tier == "ZONAL" && size(self.fileShareName) <= 32), message="ZONAL tier fileShareName length must be 32 or less characters"
-// +kubebuilder:validation:XValidation:rule=(self.tier != "BASIC_SSD" || self.tier == "BASIC_SSD" && self.capacityGb >= 2560 && self.capacityGb <= 65433), message="BASIC_SSD tier capacityGb must be between 2560 and 65433"
-// +kubebuilder:validation:XValidation:rule=(self.tier != "BASIC_SSD" || self.tier == "BASIC_SSD" && size(self.fileShareName) <= 16), message="BASIC_SSD tier fileShareName length must be 16 or less characters"
-// +kubebuilder:validation:XValidation:rule=(self.tier != "BASIC_SSD" || self.tier == "BASIC_SSD" && self.capacityGb >= oldSelf.capacityGb), message="BASIC_SSD tier capacityGb cannot be reduced"
-// +kubebuilder:validation:XValidation:rule=(self.tier != "BASIC_HDD" || self.tier == "BASIC_HDD" && self.capacityGb >= 1024 && self.capacityGb <= 65433), message="BASIC_HDD tier capacityGb must be between 1024 and 65433"
-// +kubebuilder:validation:XValidation:rule=(self.tier != "BASIC_HDD" || self.tier == "BASIC_HDD" && size(self.fileShareName) <= 16), message="BASIC_HDD tier fileShareName length must be 16 or less characters"
-// +kubebuilder:validation:XValidation:rule=(self.tier != "BASIC_HDD" || self.tier == "BASIC_HDD" && self.capacityGb >= oldSelf.capacityGb), message="BASIC_HDD tier capacityGb cannot be reduced"
+// +kubebuilder:validation:XValidation:rule=(self.tier != "REGIONAL" || (self.capacityGb >= 1024 && self.capacityGb <= 9984 && (self.capacityGb - 1024) % 256 == 0 || self.capacityGb >= 10240 && self.capacityGb <= 102400 && (self.capacityGb - 10240) % 2560 == 0)), message="REGIONAL tier capacityGb must be between 1024 and 9984, and it must be divisble by 256, or between 10240 and 102400, and divisible by 2560"
+// +kubebuilder:validation:XValidation:rule=(self.tier != "REGIONAL" || size(self.fileShareName) <= 32), message="REGIONAL tier fileShareName length must be 32 or less characters"
+// +kubebuilder:validation:XValidation:rule=(self.tier != "ZONAL" || (self.capacityGb >= 1024 && self.capacityGb <= 9984 && (self.capacityGb - 1024) % 256 == 0 || self.capacityGb >= 10240 && self.capacityGb <= 102400 && (self.capacityGb - 10240) % 2560 == 0)), message="ZONAL tier capacityGb must be between 1024 and 9984, and it must be divisble by 256, or between 10240 and 102400, and divisible by 2560"
+// +kubebuilder:validation:XValidation:rule=(self.tier != "ZONAL" || size(self.fileShareName) <= 32), message="ZONAL tier fileShareName length must be 32 or less characters"
+// +kubebuilder:validation:XValidation:rule=(self.tier != "BASIC_SSD" || (self.capacityGb >= 2560 && self.capacityGb <= 65433)), message="BASIC_SSD tier capacityGb must be between 2560 and 65433"
+// +kubebuilder:validation:XValidation:rule=(self.tier != "BASIC_SSD" || size(self.fileShareName) <= 16), message="BASIC_SSD tier fileShareName length must be 16 or less characters"
+// +kubebuilder:validation:XValidation:rule=(self.tier != "BASIC_SSD" || self.capacityGb >= oldSelf.capacityGb), message="BASIC_SSD tier capacityGb cannot be reduced"
+// +kubebuilder:validation:XValidation:rule=(self.tier != "BASIC_HDD" || (self.capacityGb >= 1024 && self.capacityGb <= 65433)), message="BASIC_HDD tier capacityGb must be between 1024 and 65433"
+// +kubebuilder:validation:XValidation:rule=(self.tier != "BASIC_HDD" || size(self.fileShareName) <= 16), message="BASIC_HDD tier fileShareName length must be 16 or less characters"
+// +kubebuilder:validation:XValidation:rule=(self.tier != "BASIC_HDD" || self.capacityGb >= oldSelf.capacityGb), message="BASIC_HDD tier capacityGb cannot be reduced"
 type GcpNfsVolumeSpec struct {
 	// +optional
 	// +kubebuilder:validation:XValidation:rule=(self == oldSelf), message="IpRange is immutable."
