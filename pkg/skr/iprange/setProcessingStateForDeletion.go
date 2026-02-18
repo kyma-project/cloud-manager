4cc5cbf0d222b765c9c161bf08c4ce9a52ff358b
Stefan Kikic <stefan.kikic@sap.com>
2026-02-18 13:11:18 +0100

    fix: Handle status update failures in setProcessingStateForDeletion

--- pkg/skr/iprange/setProcessingStateForDeletion.go
+++ pkg/skr/iprange/setProcessingStateForDeletion.go
@@ -12,7 +12,7 @@ func setProcessingStateForDeletion(ctx context.Context, st composed.State) (erro
 
 	if !composed.MarkedForDeletionPredicate(ctx, st) {
 		// SKR IpRange is NOT marked for deletion, do not delete mirror in KCP
-		return nil, ctx
+		return nil, nil
 	}
 	if state.KcpIpRange == nil || composed.IsMarkedForDeletion(state.KcpIpRange) {
 		return nil, nil // KCP IpRange is already marked for deletion, so it already passed Processing state
@@ -26,8 +26,7 @@ func setProcessingStateForDeletion(ctx context.Context, st composed.State) (erro
 	state.ObjAsIpRange().SetState(cloudresourcesv1beta1.StateProcessing)
 	err := state.UpdateObjStatus(ctx)
 	if err != nil {
-		// No reason to halt the flow if we can't set the processing state here as it will go to "deleting" or "error" state soon
-		return composed.LogErrorAndReturn(err, "Error updating SKR IpRange status with Processing state", nil, ctx)
+		return composed.LogErrorAndReturn(err, "Error updating SKR IpRange status with Processing state", composed.StopWithRequeue, ctx)
 	}
 	return nil, nil
 }
