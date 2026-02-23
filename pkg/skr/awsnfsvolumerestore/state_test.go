366c03b3f6c7a691087ee09536618e2150a5e7cc
Stefan Kikic <stefan.kikic@sap.com>
2026-02-23 10:06:48 +0100

    fix: AWS NFS Volume restore to existing filesystem

--- pkg/skr/awsnfsvolumerestore/state_test.go
+++ pkg/skr/awsnfsvolumerestore/state_test.go
@@ -55,7 +55,7 @@ var awsNfsVolume = cloudresourcesv1beta1.AwsNfsVolume{
 	},
 	Status: cloudresourcesv1beta1.AwsNfsVolumeStatus{
 		Id:     "test-aws-nfs-instance",
-		Server: "10.20.30.2",
+		Server: "fs-test123.efs.us-east-1.amazonaws.com",
 		Conditions: []metav1.Condition{
 			{
 				Type:               "Ready",
