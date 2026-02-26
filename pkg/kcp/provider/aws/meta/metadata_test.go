da9ef176a413e6ee8416be01c27c5abf3fe4b07a
Stefan Kikic <stefan.kikic@sap.com>
2026-02-26 12:40:40 +0100

    test: add unit test for IsInvalidParameter function

--- pkg/kcp/provider/aws/meta/metadata_test.go
+++ pkg/kcp/provider/aws/meta/metadata_test.go
@@ -2,6 +2,7 @@ package meta
 
 import (
 	"errors"
+	backuptypes "github.com/aws/aws-sdk-go-v2/service/backup/types"
 	"github.com/aws/smithy-go"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -31,4 +32,33 @@ func TestIsNotFound(t *testing.T) {
 			assert.True(t, IsNotFound(&smithy.GenericAPIError{Code: code}))
 		})
 	}
+}
+
+func TestIsInvalidParameter(t *testing.T) {
+	t.Run("returns true for InvalidParameterValueException", func(t *testing.T) {
+		err := &backuptypes.InvalidParameterValueException{
+			Message: strPtr("Idempotency token already used"),
+		}
+		assert.True(t, IsInvalidParameter(err))
+	})
+
+	t.Run("returns false for nil error", func(t *testing.T) {
+		assert.False(t, IsInvalidParameter(nil))
+	})
+
+	t.Run("returns false for other error types", func(t *testing.T) {
+		err := errors.New("some other error")
+		assert.False(t, IsInvalidParameter(err))
+	})
+
+	t.Run("returns false for wrapped non-matching error", func(t *testing.T) {
+		err := &backuptypes.ResourceNotFoundException{
+			Message: strPtr("not found"),
+		}
+		assert.False(t, IsInvalidParameter(err))
+	})
+}
+
+func strPtr(s string) *string {
+	return &s
 }
