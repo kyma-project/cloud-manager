cc4c70bc443db4586ee2a279578f4d64e3f608a2
Milos Tomic <59831542+tmilos77@users.noreply.github.com>
2026-03-19 13:32:23 +0100

    feat: openstack application credentials (#1763)

--- pkg/kcp/provider/sap/config/config.go
+++ pkg/kcp/provider/sap/config/config.go
@@ -6,6 +6,10 @@ type SapConfigStruct struct {
 	Username string `json:"username" yaml:"username"`
 	Password string `json:"password" yaml:"password"`
 
+	ApplicationCredentialID     string `json:"applicationCredentialID" yaml:"applicationCredentialID"`
+	ApplicationCredentialName   string `json:"applicationCredentialName" yaml:"applicationCredentialName"`
+	ApplicationCredentialSecret string `json:"applicationCredentialSecret" yaml:"applicationCredentialSecret"`
+
 	FloatingPoolNetwork string `json:"floatingPoolNetwork" yaml:"floatingPoolNetwork"`
 	FloatingPoolSubnet  string `json:"floatingPoolSubnet" yaml:"floatingPoolSubnet"`
 }
@@ -29,12 +33,32 @@ func InitConfig(cfg config.Config) {
 			config.SourceFile("OS_PASSWORD"),
 		),
 		config.Path(
+			"applicationCredentialID",
+			config.SourceEnv("OS_APPLICATION_CREDENTIAL_ID"),
+			config.SourceFile("OS_APPLICATION_CREDENTIAL_ID"),
+		),
+		config.Path(
+			"applicationCredentialName",
+			config.SourceEnv("OS_APPLICATION_CREDENTIAL_NAME"),
+			config.SourceFile("OS_APPLICATION_CREDENTIAL_NAME"),
+		),
+		config.Path(
+			"applicationCredentialSecret",
+			config.Sensitive(),
+			config.SourceEnv("OS_APPLICATION_CREDENTIAL_SECRET"),
+			config.SourceFile("OS_APPLICATION_CREDENTIAL_SECRET"),
+		),
+		config.Path(
 			"floatingPoolNetwork",
 			config.DefaultScalar("FloatingIP-external-kyma-01"),
+			config.SourceEnv("OS_FLOATING_POOL_NETWORK"),
+			config.SourceFile("OS_FLOATING_POOL_NETWORK"),
 		),
 		config.Path(
 			"floatingPoolSubnet",
 			config.DefaultScalar("FloatingIP-internet-*"),
+			config.SourceEnv("OS_FLOATING_POOL_SUBNET"),
+			config.SourceFile("OS_FLOATING_POOL_SUBNET"),
 		),
 	)
 }
