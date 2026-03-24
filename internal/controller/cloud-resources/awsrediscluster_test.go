9e005b23e24e90c57010e18e8832ad5089a2277f
Stefan Kikic <stefan.kikic@sap.com>
2026-03-25 00:54:57 +0100

    chore: modernize internal controllers with Go 1.26 improvements
    Apply go fix automated improvements to internal controller tests:
    - Replace interface{} with any keyword
    - Use slices.Contains() from stdlib
    - Use modern Go 1.26 idioms
    
    Files: 7 (internal/controller/*)

--- internal/controller/cloud-resources/awsrediscluster_test.go
+++ internal/controller/cloud-resources/awsrediscluster_test.go
@@ -15,7 +15,6 @@ import (
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
-	"k8s.io/utils/ptr"
 )
 
 var _ = Describe("Feature: SKR AwsRedisCluster", func() {
@@ -68,7 +67,7 @@ var _ = Describe("Feature: SKR AwsRedisCluster", func() {
 		shardCount := int32(10)
 		replicasPerShard := int32(2)
 
-		preferredMaintenanceWindow := ptr.To("sun:23:00-mon:01:30")
+		preferredMaintenanceWindow := new("sun:23:00-mon:01:30")
 
 		parameterKey := "active-defrag-cycle-max"
 		parameterValue := "85"
