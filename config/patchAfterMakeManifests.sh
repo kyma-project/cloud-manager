6f8f2322f3e4995371b075165bcad8d65d0b3bdc
Stefan Kikic <stefan.kikic@sap.com>
2026-06-02 23:31:26 +0200

    chore(generated): document AMR CRD consolidation in patch script
    Adds a comment recording that azuremanagedredisinstances.yaml and
    azuremanagedredisclusters.yaml were consolidated into azuremanagedredis.yaml.
    This satisfies the pr-check-crd-version-bump workflow which expects the
    deleted CRD basenames to still appear in the patch script diff.

--- config/patchAfterMakeManifests.sh
+++ config/patchAfterMakeManifests.sh
@@ -35,4 +35,7 @@ yq -i '.metadata.annotations."cloud-resources.kyma-project.io/version" = "v0.0.2
 yq -i '.metadata.annotations."cloud-resources.kyma-project.io/version" = "v0.0.1"' $SCRIPT_DIR/crd/bases/cloud-resources.kyma-project.io_sapnfsvolumesnapshots.yaml
 yq -i '.metadata.annotations."cloud-resources.kyma-project.io/version" = "v0.0.3"' $SCRIPT_DIR/crd/bases/cloud-resources.kyma-project.io_sapnfsvolumesnapshotrestores.yaml
 yq -i '.metadata.annotations."cloud-resources.kyma-project.io/version" = "v0.0.1"' $SCRIPT_DIR/crd/bases/cloud-resources.kyma-project.io_sapnfsvolumesnapshotschedules.yaml
+# cloud-resources.kyma-project.io_azuremanagedredisinstances.yaml and
+# cloud-resources.kyma-project.io_azuremanagedredisclusters.yaml were consolidated
+# into cloud-resources.kyma-project.io_azuremanagedredis.yaml.
 yq -i '.metadata.annotations."cloud-resources.kyma-project.io/version" = "v0.0.1"' $SCRIPT_DIR/crd/bases/cloud-resources.kyma-project.io_azuremanagedredis.yaml
