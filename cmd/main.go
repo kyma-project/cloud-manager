b71533c147f10606a505522dcd92ea6888e67533
Stefan Kikic <stefan.kikic@sap.com>
2026-07-02 15:44:13 +0200

    fix(backupschedule): thread clock.Clock through controller Setup functions
    Add clock.Clock parameter to SetupAwsNfsBackupScheduleReconciler and
    SetupAzureRwxBackupScheduleReconciler, fix missing argument in GCP v1
    fallback path, and wire clock.RealClock{} for all three in cmd/main.go.

--- cmd/main.go
+++ cmd/main.go
@@ -362,7 +362,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = cloudresourcescontroller.SetupAwsNfsBackupScheduleReconciler(skrRegistry, env); err != nil {
+	if err = cloudresourcescontroller.SetupAwsNfsBackupScheduleReconciler(skrRegistry, env, clock.RealClock{}); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "AwsNfsBackupSchedule")
 		os.Exit(1)
 	}
@@ -382,7 +382,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = cloudresourcescontroller.SetupAzureRwxBackupScheduleReconciler(skrRegistry, env); err != nil {
+	if err = cloudresourcescontroller.SetupAzureRwxBackupScheduleReconciler(skrRegistry, env, clock.RealClock{}); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "AzureRwxBackupSchedule")
 		os.Exit(1)
 	}
