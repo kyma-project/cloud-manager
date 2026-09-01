03d2d9c2f74d79dd1e4c4620699edd358f46592f
Stefan Kikic <stefan.kikic@sap.com>
2026-09-01 14:16:22 +0200

    fix(alicloud/redis): change AlicloudRedisCluster engineVersion default from 5.0 to 7.0 (#2159)

--- api/cloud-resources/v1beta1/alicloudrediscluster_types.go
+++ api/cloud-resources/v1beta1/alicloudrediscluster_types.go
@@ -59,7 +59,7 @@ type AlicloudRedisClusterSpec struct {
 
 	// EngineVersion is the Redis engine version. Immutable after creation.
 	// +optional
-	// +kubebuilder:default="5.0"
+	// +kubebuilder:default="7.0"
 	// +kubebuilder:validation:Enum="5.0";"6.0";"7.0"
 	// +kubebuilder:validation:XValidation:rule=(self == oldSelf), message="engineVersion is immutable."
 	EngineVersion string `json:"engineVersion,omitempty"`
