8fa449f0a143bde9bd6cd2720e6f14dcc006b0bc
Vladimir Andjelkoski <vladimir.andjelkoski@sap.com>
2026-09-11 14:04:16 +0200

    feat(WafPolicy): go lint fix

--- pkg/skr/provider/aws/wafpolicy/util.go
+++ pkg/skr/provider/aws/wafpolicy/util.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	wafv2types "github.com/aws/aws-sdk-go-v2/service/wafv2/types"
-	kcpv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-control/v1beta1"
+	cloudcontrolv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-control/v1beta1"
 	cloudresourcesv1beta1 "github.com/kyma-project/cloud-manager/api/cloud-resources/v1beta1"
 )
 
@@ -15,7 +15,7 @@ func ScopeRegional() wafv2types.Scope {
 }
 
 // convertTags converts Cloud Manager metadata to AWS WAF tags
-func convertTags(webAcl *cloudresourcesv1beta1.WafPolicy, scope *kcpv1beta1.Scope) []wafv2types.Tag {
+func convertTags(webAcl *cloudresourcesv1beta1.WafPolicy, scope *cloudcontrolv1beta1.Scope) []wafv2types.Tag {
 	tags := []wafv2types.Tag{
 		{
 			Key:   aws.String("kyma-project.io/managed-by"),
